1dfc721fca56a13ff268c0f09cf218ed09f74312
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-19 23:13:00 +0530

    Refactor Code

---
+++ front_end/lib/root_page/root_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:ramp_up/home_page/home_page.dart';
+
+class RampUpApp extends StatelessWidget {
+  const RampUpApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RampUp App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
--- front_end/test/widget_test.dart
+++ front_end/test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:front_end/main.dart';
+import 'package:ramp_up/root_page/root_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RampUpApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
