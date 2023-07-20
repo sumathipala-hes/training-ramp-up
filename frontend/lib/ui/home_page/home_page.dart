ce7040eef492f600f3c5aeae7b3d52d549ef03ce
Sandun Induranga <sandutharu40@gmail.com>
2023-07-20 13:50:24 +0530

    Home Page Updated

--- frontend/lib/ui/home_page/home_page.dart
+++ frontend/lib/ui/home_page/home_page.dart
@@ -5,6 +5,13 @@ class HomepageView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ramp Up'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
+    );
   }
 }
