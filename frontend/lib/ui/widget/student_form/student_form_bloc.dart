bdfa1f97e2b908f155a4d769c15b29aedb633667
Sandun Induranga <sandutharu40@gmail.com>
2023-07-20 21:06:11 +0530

    Bloc Class Updated

--- frontend/lib/ui/widget/student_form/student_form_bloc.dart
+++ frontend/lib/ui/widget/student_form/student_form_bloc.dart
@@ -3,5 +3,15 @@ import 'package:frontend/ui/widget/student_form/student_form_event.dart';
 import 'package:frontend/ui/widget/student_form/student_form_state.dart';
 
 class StudentFormBloc extends Bloc<StudentFormEvent, StudentFormState> {
-  StudentFormBloc() : super(StudentFormState.initialState) {}
+  StudentFormBloc() : super(StudentFormState.initialState) {
+    on<SetRadioButtons>(_setRadioButtons);
+  }
+
+  void _setRadioButtons(SetRadioButtons event, Emitter<StudentFormState> emit) {
+    emit(
+      state.clone(
+        maleOrFemale: event.maleOrFemale,
+      ),
+    );
+  }
 }
