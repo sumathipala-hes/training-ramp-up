c42df662455cbfff759228a28d197d310a9cf5b5
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-20 21:15:04 +0530

    Refactored Code

--- front_end/lib/ui/widget/student_form.dart
+++ front_end/lib/ui/widget/student_form.dart
@@ -37,21 +37,49 @@ class _PopupModelState extends State<PopupModel> {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: const Text("Pop-up Model"),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      title: const SizedBox(
+        width: 400,
+        child: Center(
+          child: Text(
+            "ADD NEW STUDENT",
+            style: TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.blue),
+          ),
+        ),
+      ),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           TextField(
             controller: nameController,
-            decoration: const InputDecoration(labelText: "Name :"),
+            decoration: const InputDecoration(
+              labelText: "Name :",
+              labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
           TextField(
             controller: addressController,
-            decoration: const InputDecoration(labelText: "Address :"),
+            decoration: const InputDecoration(
+              labelText: "Address :",
+              labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
           TextField(
             controller: mobileNoController,
-            decoration: const InputDecoration(labelText: "Mobile :"),
+            decoration: const InputDecoration(
+              labelText: "Mobile :",
+              labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold),
+            ),
           ),
           TextFormField(
             controller: dateController,
@@ -59,35 +87,59 @@ class _PopupModelState extends State<PopupModel> {
               _selectDate(context);
             },
             decoration: const InputDecoration(
-              labelText: "Date",
+              labelText: "Date :",
+              labelStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold),
               suffixIcon: Icon(Icons.calendar_today),
             ),
           ),
           const SizedBox(height: 16),
-          Row(
-            children: [
-              Radio(
-                value: "Male",
-                groupValue: selectedGender,
-                onChanged: (value) {
-                  setState(() {
-                    selectedGender = value!;
-                  });
-                },
-              ),
-              const Text("Male"),
-              Radio(
-                value: "Female",
-                groupValue: selectedGender,
-                onChanged: (value) {
-                  setState(() {
-                    selectedGender = value!;
-                  });
-                },
-              ),
-              const Text("Female"),
-            ],
-          ),
+          Center(
+            child: Row(
+              children: [
+                Radio(
+                  value: "Male",
+                  groupValue: selectedGender,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        selectedGender = value!;
+                      },
+                    );
+                  },
+                ),
+                const Text(
+                  "Male",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                Radio(
+                  value: "Female",
+                  groupValue: selectedGender,
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        selectedGender = value!;
+                      },
+                    );
+                  },
+                ),
+                const Text(
+                  "Female",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
       actions: [
@@ -104,17 +156,29 @@ class _PopupModelState extends State<PopupModel> {
               'Address : $address',
               'Mobile NO : $mobileNo',
             );
-            FlutterLogs.logInfo('Date : $date', 'Gender : $gender', '',);
+            FlutterLogs.logInfo(
+              'Date : $date',
+              'Gender : $gender',
+              '',
+            );
 
             Navigator.of(context).pop();
           },
-          child: const Text("Save"),
+          child: const Text(
+            "SAVE",
+            style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.blue),
+          ),
         ),
         TextButton(
           onPressed: () {
             Navigator.of(context).pop();
           },
-          child: const Text("Cancel"),
+          child: const Text(
+            "CANCEL",
+            style: TextStyle(
+                fontSize: 16, fontWeight: FontWeight.bold, color: Colors.red),
+          ),
         ),
       ],
     );
