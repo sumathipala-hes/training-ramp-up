6e6c4e4b1a12c88765d32a882e0dac196ede54c3
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-07-20 18:03:45 +0530

    Introducing Home page using bloc

--- front_end/lib/ui/root_page/root_page.dart
+++ front_end/lib/ui/root_page/root_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front_end/ui/ramp_up_home_page/ramp_up_home_page_provider.dart';
 
 class RampUpApp extends StatelessWidget {
   const RampUpApp({super.key});
@@ -10,13 +11,14 @@ class RampUpApp extends StatelessWidget {
       title: 'Ramp Up',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
-        useMaterial3: true,
+        // useMaterial3: true,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Ramp Up'),
-        ),
-      ),
+      // home: const Scaffold(
+      //   body: Center(
+      //     child: Text('Ramp Up'),
+      //   ),
+      // ),
+      home: RampUpHomeScreenProvider(),
     );
   }
 }
