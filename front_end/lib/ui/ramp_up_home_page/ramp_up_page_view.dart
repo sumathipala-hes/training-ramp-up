8533c7f56d8d225059371b9d6306322542edae41
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-07-21 08:18:10 +0530

    Added background img and FullyRefactored ramp up home page

--- front_end/lib/ui/ramp_up_home_page/ramp_up_page_view.dart
+++ front_end/lib/ui/ramp_up_home_page/ramp_up_page_view.dart
@@ -18,32 +18,42 @@ class RampUpHomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 30),
-          Center(
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueGrey,
-                fixedSize: const Size(170, 40),
-                shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(10),
+      body: Container(
+        height: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/img/background.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey[700],
+                  elevation: 5,
+                  fixedSize: const Size(170, 40),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  'ADD NEW STUDENT',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
               ),
-              onPressed: () {},
-              child: const Text(
-                'ADD NEW STUDENT',
-                style: TextStyle(
-                  fontSize: 15.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
