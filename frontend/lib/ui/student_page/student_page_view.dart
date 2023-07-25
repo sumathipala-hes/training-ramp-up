9a64abf33493c43a2b33901059e711d828cf34dd
Sandun Induranga <sandutharu40@gmail.com>
2023-07-26 00:06:51 +0530

    Student Page View

---
+++ frontend/lib/ui/student_page/student_page_view.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
+
+import '../home_page/home_page_bloc.dart';
+import '../home_page/home_page_event.dart';
+import '../theme/colors.dart';
+
+class StudentPageView extends StatelessWidget {
+  const StudentPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController nameController = TextEditingController();
+    final TextEditingController addressController = TextEditingController();
+    final TextEditingController mobileController = TextEditingController();
+    final TextEditingController dateController = TextEditingController();
+    DateTime dob = DateTime.now();
+    String gender = 'Male';
+
+    HomePageBloc homePageBloc = BlocProvider.of<HomePageBloc>(context);
+
+    return SingleChildScrollView(
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Add New Student',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: AppColors.mainColor,
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Name',
+                  hintText: 'Enter First Name',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: addressController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Address',
+                  hintText: 'Enter Address',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: mobileController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Mobile',
+                  hintText: 'Enter Mobile',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: dateController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  suffixIcon: Icon(
+                    Icons.calendar_today,
+                  ),
+                  labelText: "Date of Birth",
+                ),
+                readOnly: true,
+                onTap: () async {
+                  DateTime? date = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.parse('2000-01-01'),
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(DateTime.now().year - 18),
+                  );
+                  dateController.text = DateFormat('EEE MMM d yyyy').format(
+                    date ?? DateTime.now(),
+                  );
+                  dob = date ?? DateTime.now();
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Radio(
+                        value: 'Male',
+                        fillColor: MaterialStateColor.resolveWith(
+                          (states) => AppColors.mainColor,
+                        ),
+                        groupValue: gender,
+                        onChanged: (value) {
+                          // setState(() {
+                          //   gender = value.toString();
+                          // });
+                        },
+                      ),
+                      const Text('Male'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Radio(
+                        value: 'Female',
+                        fillColor: MaterialStateColor.resolveWith(
+                          (states) => AppColors.mainColor,
+                        ),
+                        groupValue: gender,
+                        onChanged: (value) {
+                          // setState(() {
+                          //   gender = value.toString();
+                          // });
+                        },
+                      ),
+                      const Text('Female'),
+                    ],
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.25,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.errorColor,
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('CANCEL'),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.25,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.successColor,
+                      ),
+                      onPressed: () async {
+                        Navigator.of(context).pop();
+                        homePageBloc.add(
+                          SaveStudentEvent(
+                            name: nameController.text,
+                            address: addressController.text,
+                            mobile: mobileController.text,
+                            dob: dob,
+                            gender: gender,
+                          ),
+                        );
+                      },
+                      child: const Text('SAVE'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/lib/util/validation_util.dart
@@ -0,0 +1,4 @@
+final RegExp nameRegExp = RegExp(r'^[A-Za-z ]+$');
+final RegExp addressRegExp = RegExp(r'^[A-Za-z0-9 ]+$');
+final RegExp mobileRegExp = RegExp(r'^[0-9]+$');
+final RegExp dobRegExp = RegExp(r'^[A-Za-z ]+$');
