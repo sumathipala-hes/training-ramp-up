2daf3968de12226d8ff9b8ae3c2869cd8d01bbb2
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-07-25 23:55:48 +0530

    Refactored

--- front_end/lib/ui/root_page/root_page.dart
+++ front_end/lib/ui/root_page/root_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:front_end/theme/primary_theme.dart';
 import 'package:front_end/ui/home_page/home_page_bloc.dart';
-// import 'package:front_end/ui/home_page/home_page_provider.dart';
 import 'package:front_end/ui/home_page/home_page_view.dart';
 
 class RampUpApp extends StatelessWidget {
