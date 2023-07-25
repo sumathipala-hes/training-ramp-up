9e491bd389bb4530eba3a692071596dad1ed1cdf
Sandun Induranga <sandutharu40@gmail.com>
2023-07-26 01:00:19 +0530

    Update And Delete UI

--- frontend/lib/ui/widget/student_card.dart
+++ frontend/lib/ui/widget/student_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/model/student.dart';
+import 'package:frontend/ui/student_page/student_page_provider.dart';
 import 'package:frontend/ui/theme/colors.dart';
 import 'package:intl/intl.dart';
 
@@ -17,47 +18,57 @@ class StudentCard extends StatefulWidget {
 class _StudentCardState extends State<StudentCard> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: MediaQuery.of(context).size.width * 0.9,
-      child: Card(
-        elevation: 4,
-        color: AppColors.mainColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                widget.student.id,
-                style: const TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
+    return ElevatedButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => StudentPageProvider(),
+          ),
+        );
+      },
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.9,
+        child: Card(
+          elevation: 4,
+          color: AppColors.mainColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(40, 20, 40, 20),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  widget.student.id,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.student.name,
-                    style: const TextStyle(
-                      fontSize: 20,
-                      color: Colors.white70,
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.student.name,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white70,
+                      ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    DateFormat('EEE MMM d yyyy').format(widget.student.dob),
-                    style: const TextStyle(color: Colors.white60),
-                  ),
-                ],
-              ),
-            ],
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      DateFormat('EEE MMM d yyyy').format(widget.student.dob),
+                      style: const TextStyle(color: Colors.white60),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
