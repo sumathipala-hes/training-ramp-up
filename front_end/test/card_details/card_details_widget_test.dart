b1ffa418134bddb42a208be062b9870513e9242a
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-07-30 19:34:48 +0530

    Add Widget Testing in card Details

---
+++ front_end/test/card_details/card_details_widget_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart'; // Don't forget to import DateFormat
+import 'package:ramp_up/model/student_model.dart';
+import 'package:ramp_up/ui/widget/card_details.dart';
+
+void main() {
+  testWidgets('StudentCard displays correct content',
+      (WidgetTester tester) async {
+    Student student = Student(
+      id: '1',
+      name: 'Dasun',
+      address: 'Galle',
+      mobileNumber: '0765675468',
+      dob: DateTime.now(),
+      gender: 'Male',
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: StudentCard(
+          id: student.id,
+          studentName: student.name,
+          studentDOB: student.dob,
+        ),
+      ),
+    );
+
+    expect(find.text(student.id), findsOneWidget);
+    expect(find.text(student.name), findsOneWidget);
+    expect(
+      find.text(DateFormat('EEE MMM d yyyy').format(student.dob)),
+      findsOneWidget,
+    );
+  });
+}
