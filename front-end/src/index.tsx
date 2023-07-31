37a437f01bbf10ea3587c296e6d05289deb99387
exagenio <123676288+exagenio@users.noreply.github.com>
2023-07-31 13:17:04 +0530

    created test cases

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
 import { Provider } from 'react-redux';
 
@@ -16,5 +15,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-reportWebVitals();
