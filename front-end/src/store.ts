c4dc06999132cb92c92fdd7e454808eba2a176ea
Abdullah Marikkar <abdullahmarikkar2@gmail.com>
2023-08-02 09:56:12 +0530

    Written Test Cases

--- front-end/src/store.ts
+++ front-end/src/store.ts
@@ -1,6 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import gridReducer from './Components/GridTable/GridSlice'
-import { persistStore, persistReducer } from 'redux-persist'
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 const persistConfig = {
@@ -16,6 +25,19 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
 })
 
 export const persistor = persistStore(store)
