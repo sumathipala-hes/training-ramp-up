c47e96a3a6f0304990316b98e6e51345216d968e
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-08-02 17:10:54 +0530

    Refactored Code

--- front_end/test/home_page/widget_test.dart
+++ front_end/test/home_page/widget_test.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:intl/intl.dart';
+import 'package:ramp_up/model/student_model.dart';
 import 'package:ramp_up/ui/home_page/home_page_bloc.dart';
+import 'package:ramp_up/ui/widget/card_details.dart';
 import 'package:ramp_up/ui/widget/student_modal.dart';
 
 void main() {
@@ -13,6 +16,35 @@ void main() {
       ),
     );
   }
+
+  testWidgets('StudentCard displays correct content',
+      (WidgetTester tester) async {
+    Student student = Student(
+      id: '1',
+      name: 'Dasun',
+      address: 'Galle',
+      mobileNumber: '0765675468',
+      dob: DateTime.now(),
+      gender: 'Male',
+    );
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: StudentCard(
+          id: student.id,
+          studentName: student.name,
+          studentDOB: student.dob,
+        ),
+      ),
+    );
+
+    expect(find.text(student.id), findsOneWidget);
+    expect(find.text(student.name), findsOneWidget);
+    expect(
+      find.text(DateFormat('EEE MMM d yyyy').format(student.dob)),
+      findsOneWidget,
+    );
+  });
 
   testWidgets('Save button should be disabled with empty or invalid input',
       (WidgetTester tester) async {
