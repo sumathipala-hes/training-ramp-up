7cb2c61efbff0844c1cc8cbbaa01729bf6b3cadb
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-08-05 01:12:50 +0530

    Add new libs

--- back_end/src/app.ts
+++ back_end/src/app.ts
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`)
 });
