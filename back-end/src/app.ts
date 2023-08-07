9c8c426fa2b355de18a77d37fc6bc7ff1ab70271
exagenio <123676288+exagenio@users.noreply.github.com>
2023-08-07 13:47:59 +0530

    config eslint, prettier and setup fold structure

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,12 +1,11 @@
-import express from "express";
+import express from 'express';
+import test from './controllers/test';
 
-const app =  express();
+const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Worl!')
-});
+app.use('/', test);
 
 app.listen(port, () => {
-  console.log(`server listening on port ${port}`)
+  console.log(`server listening on port ${port}`);
 });
