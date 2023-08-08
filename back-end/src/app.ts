9e3a696d292f0e95f458a0bd3dee0e7fcf10ad30
exagenio <123676288+exagenio@users.noreply.github.com>
2023-08-08 11:57:07 +0530

    Revert "Database Connectivity"
    This reverts commit b4a0bd82ca60ae70baaa41ac37f6c21828181165.

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,21 +1,10 @@
 import express from 'express';
-import "reflect-metadata";
-import AppDataSource from './services/dataSoure';
-import router from "./routes";
+import test from './controllers/test';
 
 const app = express();
 const port = 3000;
 
-//set routers
-app.use('/', router);
-
-//connect database
-AppDataSource.initialize()
-    .then(() => {
-      console.log("Connected to database")
-        // here you can start to work with your database
-    })
-    .catch((error) => console.log(error))
+app.use('/', test);
 
 app.listen(port, () => {
   console.log(`server listening on port ${port}`);
--- back-end/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import {initializeApp} from "../controllers/initializeApp"
-
-const router = express.Router();
-router.get('/', initializeApp);
-export default router;
