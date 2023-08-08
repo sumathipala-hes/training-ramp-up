43cc0d74aca65e8b6fbdf090ae34daf4f56bc29a
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-08-08 08:09:03 +0530

    implement student added route part in the student.routes.ts

--- back_end/src/routes/student.routes.ts
+++ back_end/src/routes/student.routes.ts
@@ -1,16 +1,17 @@
 import express, { Router } from "express";
+import StudentController from "../controllers/student.controller";
 
 export default class UserRoutes {
   private router: Router = express.Router();
+  private studentController: StudentController = new StudentController();
 
   constructor() {
     this.configRoutes();
   }
 
   private configRoutes = (): void => {
-    this.router.get("/", (req, res) => {
-      res.send("Hello World!");
-    });
+     //POST /api/v1/student
+     this.router.post("/", this.studentController.addStudent);
   };
 
   public getRouter = (): Router => {
