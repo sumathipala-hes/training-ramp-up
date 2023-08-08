e5f938a46f418d96ec799b0f882001e99aaa5437
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-08-08 07:56:28 +0530

    Added app.ts and handle cros policy

---
+++ back_end/src/app.ts
@@ -0,0 +1,35 @@
+import { config } from 'dotenv';
+// dotenv configuration
+config();
+
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+// Create the express app
+const app = express();
+
+// Here you can add more origins to allow CORS
+const allowedOrigins = ['http://localhost:3000'];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin!) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  }),
+);
+
+// if you are receiving JSON data in request-body
+app.use(express.json());
+
+// Mount the routes at /resourse URL path
+app.use('/', routes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}...!`);
+});
