07a8036ca487ee5891a6cb52281a2250a83001ff
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-08-08 08:16:32 +0530

    refactor student.controller adding res

--- back_end/src/controllers/student.controller.ts
+++ back_end/src/controllers/student.controller.ts
@@ -1,35 +1,35 @@
-import { RequestHandler, Request, Response } from "express";
+import { RequestHandler, Request, Response } from 'express';
 
 export default class StudentController {
   addStudent: RequestHandler = async (
     req: Request,
-    res: Response
+    res: Response,
   ): Promise<Response> => {
     //create operation
-    return res;
+    return res.status(201).json({ message: 'Student added successfully' });
   };
 
   retrieveAllStudents: RequestHandler = async (
     req: Request,
-    res: Response
+    res: Response,
   ): Promise<Response> => {
     //read operation
-    return res;
+    return res.status(200).json({ message: 'All students retrieved' });
   };
 
   updateStudent: RequestHandler = async (
     req: Request,
-    res: Response
+    res: Response,
   ): Promise<Response> => {
     //update operation
-    return res;
+    return res.status(200).json({ message: 'Student updated successfully' });
   };
 
   deleteStudent: RequestHandler = async (
     req: Request,
-    res: Response
+    res: Response,
   ): Promise<Response> => {
     //delete operation
-    return res;
+    return res.status(200).json({ message: 'Student deleted successfully' });
   };
 }
