3513a29613bd950b0729ebe692f15b6d46f75362
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-08-08 19:53:21 +0530

    Refactored

--- back_end/src/configs/db.config.ts
+++ back_end/src/configs/db.config.ts
@@ -5,19 +5,13 @@ import { DataSource } from 'typeorm/data-source/DataSource';
 
 export const dataSource = new DataSource({
   type: 'postgres',
-  // host: process.env.DB,
-  // port: Number(process.env.DB_PORT),
-  // username: process.env.DB_USER,
-  // password: process.env.DB_PASS,
-  // database: process.env.DB_NAME,
-  // entities: [Student],
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '1234',
-  database: 'rampupapp',
+  host: process.env.DB,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   entities: [Student],
   synchronize: true,
-  logging: true,
+  logging: false,
 });
 
