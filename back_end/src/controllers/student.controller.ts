d22d7ce4eca176f26eddade78442bf798a7a6e0c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-08-16 14:11:44 +0530

    Refactored code

--- back_end/src/controllers/student.controller.ts
+++ back_end/src/controllers/student.controller.ts
@@ -25,43 +25,43 @@ export const requestGetAllStudents: RequestHandler = async (
   }
 };
 
-export const requestCreateStudent: RequestHandler[] = [
-  ...validateStudent,
-  async (req: Request, res: Response): Promise<void> => {
-    const studentData = req.body;
-    try {
-      const newStudent = await createStudent(studentData);
-      res.status(200).json({
-        message: 'Student created successfully',
-        data: newStudent,
-      });
-    } catch (error) {
-      res.status(500).json({ message: 'Error creating student', error });
-    }
-  },
-];
+export const requestCreateStudent: RequestHandler = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  const studentData = req.body;
+  try {
+    const newStudent = await createStudent(studentData);
+    res.status(200).json({
+      message: 'Student created successfully',
+      data: newStudent,
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Error creating student', error });
+  }
+};
 
-export const requestUpdateStudent: RequestHandler[] = [
-  ...validateStudent,
-  async (req: Request, res: Response): Promise<void> => {
-    const { id } = req.params;
-    const studentData = req.body;
-    try {
-      const updatedStudent = await updateStudent(id, studentData);
+export const requestUpdateStudent: RequestHandler = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  const { id } = req.params;
+  const studentData = req.body;
+  try {
+    const updatedStudent = await updateStudent(id, studentData);
 
-      res.status(200).json({
-        message: 'Student updated successfully',
-        data: updatedStudent,
-      });
-    } catch (error: any) {
-      if (error.message === 'Student not found') {
-        res.status(400).json({ message: 'Student not found' });
-      } else {
-        res.status(500).json({ message: 'An error occurred' });
-      }
+    res.status(200).json({
+      message: 'Student updated successfully',
+      data: updatedStudent,
+    });
+  } catch (error: any) {
+    if (error.message === 'Student not found') {
+      res.status(400).json({ message: 'Student not found' });
+    } else {
+      res.status(500).json({ message: 'An error occurred' });
     }
-  },
-];
+  }
+};
 
 export const requestDeleteStudent: RequestHandler = async (
   req: Request,
