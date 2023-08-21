82bd75854801174b5202633955754f90838cb3e3
Sandun Induranga <sandutharu40@gmail.com>
2023-08-21 20:34:11 +0530

    User Home Page Updated

---
+++ frontend/lib/ui/user_home_page/user_home_page_state.dart
@@ -0,0 +1,3 @@
+class UserHomePageState {
+  UserHomePageState();
+}
---
+++ frontend/lib/ui/user_home_page/user_home_page_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserHomePageView extends StatelessWidget {
+  const UserHomePageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
