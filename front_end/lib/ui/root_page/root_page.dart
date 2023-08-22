2800c44096927ab9635f2e474316e9b9e30bf214
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-08-23 01:19:20 +0530

    refactor root page

--- front_end/lib/ui/root_page/root_page.dart
+++ front_end/lib/ui/root_page/root_page.dart
@@ -1,37 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:front_end/theme/primary_theme.dart';
-// import 'package:front_end/ui/admin_home_page/admin_home_page_bloc.dart';
-// import 'package:front_end/ui/admin_home_page/admin_home_page_view.dart';
-// import 'package:front_end/ui/home_page/home_page_bloc.dart';
-// import 'package:front_end/ui/home_page/home_page_view.dart';
-// import 'package:front_end/ui/sign_in_page/sign_in_page_view.dart';
-import 'package:front_end/ui/user_home_page/user_home_page_bloc.dart';
-import 'package:front_end/ui/user_home_page/user_home_page_view.dart';
+import 'package:front_end/ui/admin_home_page/admin_home_page_bloc.dart';
+import 'package:front_end/ui/sign_in_page/sign_in_page_provider.dart';
 
 class RampUpApp extends StatelessWidget {
   const RampUpApp({super.key});
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   final materialApp = MaterialApp(
-  //     title: 'RampUp App',
-  //     theme: PrimaryTheme.generateTheme(context),
-  //     debugShowCheckedModeBanner: false,
-  //     home: const RampUpHomeScreen(),
-  //   );
-
-  //   return MultiBlocProvider(
-  //     providers: [
-  //       BlocProvider<RampUpHomeScreenBloc>(
-  //         create: (context) => RampUpHomeScreenBloc(context),
-  //       ),
-  //     ],
-  //     child: materialApp,
-  //   );
-  // }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -39,46 +13,16 @@ class RampUpApp extends StatelessWidget {
       title: 'RampUp App',
       theme: PrimaryTheme.generateTheme(context),
       debugShowCheckedModeBanner: false,
-      home: const UserHomeScreen(),
+      home: const SignInPageProvider(),
     );
 
     return MultiBlocProvider(
       providers: [
-        BlocProvider<UserHomeScreenBloc>(
-          create: (context) => UserHomeScreenBloc(context),
+        BlocProvider<AdminHomeScreenBloc>(
+          create: (context) => AdminHomeScreenBloc(context),
         ),
       ],
       child: materialApp,
     );
   }
-
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   final materialApp = MaterialApp(
-  //     title: 'RampUp App',
-  //     theme: PrimaryTheme.generateTheme(context),
-  //     debugShowCheckedModeBanner: false,
-  //     home: const AdminHomeScreen(),
-  //   );
-
-  //   return MultiBlocProvider(
-  //     providers: [
-  //       BlocProvider<AdminHomeScreenBloc>(
-  //         create: (context) => AdminHomeScreenBloc(context),
-  //       ),
-  //     ],
-  //     child: materialApp,
-  //   );
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return MaterialApp(
-  //     title: 'RampUp App',
-  //     theme: PrimaryTheme.generateTheme(context),
-  //     debugShowCheckedModeBanner: false,
-  //     home: const SignInPageScreen(),
-  //   );
-  // }
 }
