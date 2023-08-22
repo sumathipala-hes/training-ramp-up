2893dcfa02b2c0ac13661d1ba087dc073b3e40a6
Sandun Induranga <sandutharu40@gmail.com>
2023-08-22 21:02:35 +0530

    Content Updated

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  email!: number;
+  email!: string;
 
   @Column()
   name!: string;
