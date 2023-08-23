c52ceee5ad77e169bb8c957a2cdf28db9d305426
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-08-24 01:08:20 +0530

    refactored register page

--- front_end/lib/ui/register_page/register_page_bloc.dart
+++ front_end/lib/ui/register_page/register_page_bloc.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:front_end/ui/register_page/register_page_event.dart';
@@ -7,17 +5,5 @@ import 'package:front_end/ui/register_page/register_page_state.dart';
 
 class RegisterScreenBloc
     extends Bloc<RegisterScreenEvent, RegisterScreenState> {
-  RegisterScreenBloc(BuildContext context)
-      : super(RegisterScreenState.initialState) {
-    on<SelectGender>(_onSelectGender);
-  }
-
-  Future<void> _onSelectGender(
-      SelectGender event, Emitter<RegisterScreenState> emit) async {
-    emit(
-      state.clone(
-        gender: event.gender,
-      )
-    );
-  }
+  RegisterScreenBloc(BuildContext context) : super(const RegisterScreenState());
 }
--- front_end/lib/ui/register_page/register_page_view.dart
+++ front_end/lib/ui/register_page/register_page_view.dart
@@ -1,32 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:front_end/ui/admin_home_page/admin_home_page_view.dart';
-import 'package:front_end/ui/register_page/register_page_bloc.dart';
-import 'package:front_end/ui/register_page/register_page_event.dart';
-import 'package:front_end/ui/register_page/register_page_state.dart';
 import 'package:front_end/ui/sign_in_page/sign_in_page_provider.dart';
 import 'package:front_end/ui/user_home_page/user_home_page_provider.dart';
-import 'package:intl/intl.dart';
 
-// ignore: must_be_immutable
 class RegisterScreen extends StatelessWidget {
   RegisterScreen({super.key});
 
   final TextEditingController nameController = TextEditingController();
-  final TextEditingController addressController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
-  final TextEditingController mobileNumberController = TextEditingController();
-  final TextEditingController dateController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmPasswordController =
       TextEditingController();
 
-  DateTime dob = DateTime.now();
-
   @override
   Widget build(BuildContext context) {
-    RegisterScreenBloc registerScreenBloc =
-        BlocProvider.of<RegisterScreenBloc>(context);
     return Scaffold(
       appBar: AppBar(
         elevation: 7,
@@ -70,19 +56,6 @@ class RegisterScreen extends StatelessWidget {
                       ),
                       const SizedBox(height: 15),
                       TextField(
-                        controller: addressController,
-                        decoration: const InputDecoration(
-                          labelText: "User Address:",
-                          prefixIcon: Icon(Icons.location_on),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(8.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 15),
-                      TextField(
                         controller: emailController,
                         decoration: const InputDecoration(
                           labelText: "User Email:",
@@ -96,84 +69,8 @@ class RegisterScreen extends StatelessWidget {
                       ),
                       const SizedBox(height: 15),
                       TextField(
-                        controller: mobileNumberController,
-                        decoration: const InputDecoration(
-                          labelText: "Mobile:",
-                          prefixIcon: Icon(Icons.mobile_friendly_rounded),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(8.0),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            flex: 2,
-                            child: TextFormField(
-                              controller: dateController,
-                              readOnly: true,
-                              onTap: () => _selectDate(context),
-                              decoration: const InputDecoration(
-                                labelText: "Date of Birth:",
-                                suffixIcon: Icon(Icons.calendar_today),
-                                border: InputBorder.none,
-                                contentPadding: EdgeInsets.symmetric(
-                                  vertical: 12.0,
-                                  horizontal: 14.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 25),
-                          Expanded(
-                            flex: 3,
-                            child: BlocBuilder<RegisterScreenBloc,
-                                RegisterScreenState>(
-                              buildWhen: (previous, current) =>
-                                  current.gender != previous.gender,
-                              builder: (context, state) {
-                                return Row(
-                                  children: [
-                                    Radio(
-                                      value: "Male",
-                                      groupValue:
-                                          registerScreenBloc.state.gender,
-                                      onChanged: (value) {
-                                        registerScreenBloc.add(
-                                          SelectGender(
-                                            gender: value.toString(),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                    const Text("Male"),
-                                    Radio(
-                                      value: "Female",
-                                      groupValue:
-                                          registerScreenBloc.state.gender,
-                                      onChanged: (value) {
-                                        registerScreenBloc.add(
-                                          SelectGender(
-                                            gender: value.toString(),
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                    const Text("Female"),
-                                  ],
-                                );
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: 15),
-                      TextField(
                         controller: passwordController,
+                        obscureText: true,
                         decoration: const InputDecoration(
                           labelText: "Create Password:",
                           prefixIcon: Icon(Icons.lock),
@@ -187,6 +84,7 @@ class RegisterScreen extends StatelessWidget {
                       const SizedBox(height: 15),
                       TextField(
                         controller: confirmPasswordController,
+                        obscureText: true,
                         decoration: const InputDecoration(
                           labelText: "Confirm Password:",
                           prefixIcon: Icon(Icons.lock),
@@ -252,8 +150,7 @@ class RegisterScreen extends StatelessWidget {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) =>
-                                      SignInPageProvider(),
+                                  builder: (context) => SignInPageProvider(),
                                 ),
                               );
                             },
@@ -277,19 +174,5 @@ class RegisterScreen extends StatelessWidget {
         ),
       ),
     );
-  }
-
-  _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime(DateTime.now().year - 28),
-      firstDate: DateTime(DateTime.now().year - 60),
-      lastDate: DateTime(DateTime.now().year - 28),
-    );
-
-    if (picked != null) {
-      dateController.text = DateFormat('EEE MMM d yyyy').format(picked);
-      dob = picked;
-    }
   }
 }
