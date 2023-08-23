3d2542e070c9e4a942be0f30b4f870086ca03c3f
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-08-23 10:26:45 +0530

    Add user repo

--- front_end/lib/repository/user_repository.dart
+++ front_end/lib/repository/user_repository.dart
@@ -18,6 +18,16 @@ class UserRepository {
     return response;
   }
 
+  Future<http.Response> getUserByOne(String search) async {
+    final response = await http.get(
+      Uri.parse('$baseUrl/users/$search'),
+      headers: <String, String>{
+        'Content-Type': 'application/json',
+      },
+    );
+    return response;
+  }
+
   Future<void> saveUser(User user) async {
     try {
       final response = await http.post(
@@ -35,6 +45,38 @@ class UserRepository {
       Logger().e('Error creating User: $error');
       showFieldError(
           'Error creating User: $error. Please contact the administrator or re-try register.');
+    }
+  }
+
+  Future<void> updateUser(User user) async {
+    try {
+      final response = await http.put(
+        Uri.parse('$baseUrl/users/${user.email}'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(user.toJson()),
+      );
+
+      if (response.statusCode != 201) {
+        Logger().d('Successfully to update User: ${response.statusCode}');
+      }
+    } catch (error) {
+      Logger().e('Error updating user: $error');
+    }
+  }
+
+  Future<void> deleteUser(String userEmail) async {
+    try {
+      final response = await http.delete(
+        Uri.parse('$baseUrl/users/$userEmail'),
+      );
+
+      if (response.statusCode != 201) {
+        Logger().d('Successfully to delete User: ${response.statusCode}');
+      }
+    } catch (error) {
+      Logger().e('Error deleting user: $error');
     }
   }
 }
