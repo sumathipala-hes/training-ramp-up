6a80c5b9ccc64f3e17c3bd8c536f9ab2bcb74b8d
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-08-24 23:33:06 +0530

    refactoed jwt config

--- back_end/src/configs/jwt.config.ts
+++ back_end/src/configs/jwt.config.ts
@@ -5,12 +5,12 @@ interface JwtConfig {
   secretKey: string;
   refreshKey: string;
   userKey: string;
-  expiresIn?: string;
+  expiresIn: string;
 }
 
 export const jwtConfig: JwtConfig = {
   secretKey: process.env.JWT_SECRET_KEY!,
   refreshKey: process.env.JWT_REFRESH_KEY!,
   userKey: process.env.JWT_USER_KEY!,
-  expiresIn: process.env.JWT_EXPIRES_IN,
+  expiresIn: process.env.JWT_EXPIRES_IN!,
 };
