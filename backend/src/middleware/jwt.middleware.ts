b1dbb3826f681b6da209946bda598ce5ddb33dee
Sandun Induranga <sandutharu40@gmail.com>
2023-08-29 15:04:34 +0530

    Updated

--- backend/src/middleware/jwt.middleware.ts
+++ backend/src/middleware/jwt.middleware.ts
@@ -1,31 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { jwtConfig } from '../configs/jwt.config';
-import { User } from '../models/user.model';
-
-export const authenticatePermissions = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): void => {
-  const user = req.cookies.user;
-  if (!user) {
-    res.sendStatus(403);
-  } else {
-    try {
-      if (req.method == 'GET') {
-        return next();
-      } else {
-        const payload = jwt.verify(user, jwtConfig.secretKey);
-        const userT = payload as User;
-        const role = userT.role;
-        role == 'Admin' ? next() : res.sendStatus(401);
-      }
-    } catch {
-      res.sendStatus(403);
-    }
-  }
-};
 
 export const authorization = (
   req: Request,
