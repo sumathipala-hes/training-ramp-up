ca17d86fba64c5afa36a7ea3d84492d2a17b71d7
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-08-29 15:05:58 +0530

    Refactored code and bug fixeds

--- back_end/src/middleware/jwt.middleware.ts
+++ back_end/src/middleware/jwt.middleware.ts
@@ -1,38 +1,6 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { jwtConfig } from '../configs/jwt.config';
-import { User } from '../models/user.models';
-
-export const generateToken = (userId: string): string => {
-  const token = jwt.sign({ sub: userId }, jwtConfig.secretKey, {
-    expiresIn: jwtConfig.expiresIn,
-  });
-  return token;
-};
-
-export const authenticatePermissions = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-): void => {
-  const user = req.cookies.user;
-  if (!user) {
-    res.sendStatus(403);
-  } else {
-    try {
-      if (req.method == 'GET') {
-        return next();
-      } else {
-        const payload = jwt.verify(user, jwtConfig.secretKey);
-        const userT = payload as User;
-        const role = userT.roleType;
-        role == 'ADMIN' ? next() : res.sendStatus(401);
-      }
-    } catch {
-      res.sendStatus(403);
-    }
-  }
-};
 
 export const authorizationPermissions = (
   req: Request,
