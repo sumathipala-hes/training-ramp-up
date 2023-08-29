2c277c46d0469d2de2b24fd19211304c6385c907
Sandun Induranga <sandutharu40@gmail.com>
2023-08-29 09:14:51 +0530

    Added Util Class

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:frontend/util/local_storage.dart';
 import 'package:frontend/ui/root_page/root_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  final prefs = await SharedPreferences.getInstance();
-  final email = prefs.getString('email');
+  bool? isAuthenticate = await LocalStorage().getDetails();
   runApp(
     RampUpApp(
-      isAuthenticate: email != null,
+      isAuthenticate: isAuthenticate ?? false,
     ),
   );
 }
