5a83428729afd4cf1440e90d3b8f9c2ec8a14a06
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-09-01 12:57:01 +0530

    refactored test cases

--- back_end/src/controllers/test/student.controller.spec.ts
+++ back_end/src/controllers/test/student.controller.spec.ts
@@ -1,135 +1,135 @@
-// import { DeleteResult, UpdateResult } from 'typeorm';
-// import { dataSource } from '../../configs/db.config';
-// import { Student } from '../../models/student.model';
-// import { sendNotification } from '../../util/notification.util';
-// import {
-//   retrieveAllStudents,
-//   saveStudent,
-//   updateStudent,
-//   deleteStudent,
-// } from '../../services/student.service';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { dataSource } from '../../configs/db.config';
+import { Student } from '../../models/student.model';
+import { sendNotification } from '../../util/notification.util';
+import {
+  retrieveAllStudents,
+  saveStudent,
+  updateStudent,
+  deleteStudent,
+} from '../../services/student.service';
 
-// jest.mock('../../util/notification.util', () => ({
-//   sendNotification: jest.fn(),
-// }));
+jest.mock('../../util/notification.util', () => ({
+  sendNotification: jest.fn(),
+}));
 
-// describe('Student Controller', () => {
-//   const studentRepo = dataSource.manager.getRepository(Student);
+describe('Student Controller', () => {
+  const studentRepo = dataSource.manager.getRepository(Student);
 
-//   describe('Retrieve All students', () => {
-//     const allStudents = [
-//       {
-//         studentId: 1,
-//         studentName: 'Maneesha',
-//         studentAddress: 'Panadura',
-//         studentMobile: '0717133074',
-//         studentDob: new Date('2001-01-01'),
-//         studentGender: 'Male',
-//       },
-//     ];
+  describe('Retrieve All students', () => {
+    const allStudents = [
+      {
+        studentId: 1,
+        studentName: 'Maneesha',
+        studentAddress: 'Panadura',
+        studentMobile: '0717133074',
+        studentDob: new Date('2001-01-01'),
+        studentGender: 'Male',
+      },
+    ];
 
-//     test('should retrieve all students successfully', async () => {
-//       studentRepo.find = jest.fn().mockResolvedValue(allStudents);
-//       const data = await retrieveAllStudents();
-//       expect(data).toEqual(allStudents);
-//     });
+    test('should retrieve all students successfully', async () => {
+      studentRepo.find = jest.fn().mockResolvedValue(allStudents);
+      const data = await retrieveAllStudents();
+      expect(data).toEqual(allStudents);
+    });
 
-//     test('should handle failure to retrieve all students', async () => {
-//       studentRepo.find = jest.fn().mockRejectedValue(new Error('Error'));
-//       await expect(retrieveAllStudents()).rejects.toThrowError('Error');
-//     });
-//   });
+    test('should handle failure to retrieve all students', async () => {
+      studentRepo.find = jest.fn().mockRejectedValue(new Error('Error'));
+      await expect(retrieveAllStudents()).rejects.toThrowError('Error');
+    });
+  });
 
-//   describe('Create student', () => {
-//     const newStudent = {
-//       studentId: 1,
-//       studentName: 'Pahasara',
-//       studentAddress: 'Galle',
-//       studentMobile: '0717133074',
-//       studentDob: new Date('2001-08-04'),
-//       studentGender: 'Male',
-//     };
+  describe('Create student', () => {
+    const newStudent = {
+      studentId: 1,
+      studentName: 'Pahasara',
+      studentAddress: 'Galle',
+      studentMobile: '0717133074',
+      studentDob: new Date('2001-08-04'),
+      studentGender: 'Male',
+    };
 
-//     test('should create student successfully', async () => {
-//       studentRepo.insert = jest.fn().mockResolvedValue(newStudent);
-//       const data = await saveStudent(newStudent);
-//       expect(data).toEqual(newStudent);
-//       expect(sendNotification).toHaveBeenCalledWith(
-//         'Success',
-//         'student Saved..!',
-//       );
-//     });
+    test('should create student successfully', async () => {
+      studentRepo.insert = jest.fn().mockResolvedValue(newStudent);
+      const data = await saveStudent(newStudent);
+      expect(data).toEqual(newStudent);
+      expect(sendNotification).toHaveBeenCalledWith(
+        'Success',
+        'student Saved..!',
+      );
+    });
 
-//     test('should handle failure to create student', async () => {
-//       studentRepo.insert = jest.fn().mockRejectedValue(new Error('Error'));
-//       await expect(saveStudent(newStudent)).rejects.toThrowError('Error');
-//     });
-//   });
+    test('should handle failure to create student', async () => {
+      studentRepo.insert = jest.fn().mockRejectedValue(new Error('Error'));
+      await expect(saveStudent(newStudent)).rejects.toThrowError('Error');
+    });
+  });
 
-//   describe('Update student', () => {
-//     const updatedStudent: Student = {
-//       studentId: 1,
-//       studentName: 'Pahasara',
-//       studentAddress: 'Panadura',
-//       studentMobile: '0717133074',
-//       studentDob: new Date('2001-08-04'),
-//       studentGender: 'Male',
-//     };
+  describe('Update student', () => {
+    const updatedStudent: Student = {
+      studentId: 1,
+      studentName: 'Pahasara',
+      studentAddress: 'Panadura',
+      studentMobile: '0717133074',
+      studentDob: new Date('2001-08-04'),
+      studentGender: 'Male',
+    };
 
-//     test('should update student successfully', async () => {
-//       const mockUpdateResult: UpdateResult = {
-//         affected: 1,
-//         raw: updatedStudent,
-//         generatedMaps: [],
-//       };
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         update: jest.fn().mockResolvedValue(mockUpdateResult),
-//       });
+    test('should update student successfully', async () => {
+      const mockUpdateResult: UpdateResult = {
+        affected: 1,
+        raw: updatedStudent,
+        generatedMaps: [],
+      };
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        update: jest.fn().mockResolvedValue(mockUpdateResult),
+      });
 
-//       const result = await updateStudent('1', updatedStudent);
-//       expect(result).toEqual(mockUpdateResult);
-//       expect(sendNotification).toHaveBeenCalledWith(
-//         'Success',
-//         'student Updated..!',
-//       );
-//     });
+      const result = await updateStudent('1', updatedStudent);
+      expect(result).toEqual(mockUpdateResult);
+      expect(sendNotification).toHaveBeenCalledWith(
+        'Success',
+        'student Updated..!',
+      );
+    });
 
-//     test('should handle failure to update student', async () => {
-//       const errorMessage = 'Error updating student.';
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         update: jest.fn().mockRejectedValue(new Error(errorMessage)),
-//       });
+    test('should handle failure to update student', async () => {
+      const errorMessage = 'Error updating student.';
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        update: jest.fn().mockRejectedValue(new Error(errorMessage)),
+      });
 
-//       await expect(updateStudent('1', updatedStudent)).rejects.toThrowError(
-//         errorMessage,
-//       );
-//     });
-//   });
+      await expect(updateStudent('1', updatedStudent)).rejects.toThrowError(
+        errorMessage,
+      );
+    });
+  });
 
-//   describe('Delete student', () => {
-//     const id = '1';
+  describe('Delete student', () => {
+    const id = '1';
 
-//     test('should delete student successfully', async () => {
-//       const mockDeleteResult: DeleteResult = {
-//         affected: 1,
-//         raw: undefined,
-//       }; // Mimic successful delete
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         delete: jest.fn().mockResolvedValue(mockDeleteResult),
-//       });
+    test('should delete student successfully', async () => {
+      const mockDeleteResult: DeleteResult = {
+        affected: 1,
+        raw: undefined,
+      }; // Mimic successful delete
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        delete: jest.fn().mockResolvedValue(mockDeleteResult),
+      });
 
-//       const result = await deleteStudent(id);
-//       expect(result).toEqual(mockDeleteResult);
-//       expect(sendNotification).toHaveBeenCalledWith('', 'student Deleted..!');
-//     });
+      const result = await deleteStudent(id);
+      expect(result).toEqual(mockDeleteResult);
+      expect(sendNotification).toHaveBeenCalledWith('', 'student Deleted..!');
+    });
 
-//     test('should handle failure to delete student', async () => {
-//       const errorMessage = 'Error deleting student.';
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         delete: jest.fn().mockRejectedValue(new Error(errorMessage)),
-//       });
+    test('should handle failure to delete student', async () => {
+      const errorMessage = 'Error deleting student.';
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        delete: jest.fn().mockRejectedValue(new Error(errorMessage)),
+      });
 
-//       await expect(deleteStudent(id)).rejects.toThrowError(errorMessage);
-//     });
-//   });
-// });
+      await expect(deleteStudent(id)).rejects.toThrowError(errorMessage);
+    });
+  });
+});
--- back_end/src/controllers/test/user.controller.spec.ts
+++ back_end/src/controllers/test/user.controller.spec.ts
@@ -1,162 +1,162 @@
-// import { DeleteResult, UpdateResult } from 'typeorm';
-// import { dataSource } from '../../configs/db.config';
-// import { User } from '../../models/user.model';
-// import { sendNotification } from '../../util/notification.util';
-// import {
-//   retrieveAllUsers,
-//   registerUser,
-//   updateUser,
-//   deleteUser,
-//   signInUser,
-// } from '../../services/user.service';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { dataSource } from '../../configs/db.config';
+import { User } from '../../models/user.model';
+import { sendNotification } from '../../util/notification.util';
+import {
+  retrieveAllUsers,
+  registerUser,
+  updateUser,
+  deleteUser,
+  signInUser,
+} from '../../services/user.service';
 
-// jest.mock('../../util/notification.util', () => ({
-//   sendNotification: jest.fn(),
-// }));
+jest.mock('../../util/notification.util', () => ({
+  sendNotification: jest.fn(),
+}));
 
-// describe('User Controller', () => {
-//   const userRepo = dataSource.manager.getRepository(User);
+describe('User Controller', () => {
+  const userRepo = dataSource.manager.getRepository(User);
 
-//   describe('Retrieve All users', () => {
-//     const allUsers = [
-//       {
-//         userName: 'Pahasara',
-//         userEmail: 'pahasara@gmail.com',
-//         userPassword: '12345678',
-//         role: 'Admin',
-//       },
-//     ];
+  describe('Retrieve All users', () => {
+    const allUsers = [
+      {
+        userName: 'Pahasara',
+        userEmail: 'pahasara@gmail.com',
+        userPassword: '12345678',
+        role: 'Admin',
+      },
+    ];
 
-//     test('should retrieve all users successfully', async () => {
-//       userRepo.find = jest.fn().mockResolvedValue(allUsers);
-//       const data = await retrieveAllUsers();
-//       expect(data).toEqual(allUsers);
-//     });
+    test('should retrieve all users successfully', async () => {
+      userRepo.find = jest.fn().mockResolvedValue(allUsers);
+      const data = await retrieveAllUsers();
+      expect(data).toEqual(allUsers);
+    });
 
-//     test('should handle failure to retrieve all users', async () => {
-//       userRepo.find = jest.fn().mockRejectedValue(new Error('Error'));
-//       await expect(retrieveAllUsers()).rejects.toThrowError('Error');
-//     });
-//   });
+    test('should handle failure to retrieve all users', async () => {
+      userRepo.find = jest.fn().mockRejectedValue(new Error('Error'));
+      await expect(retrieveAllUsers()).rejects.toThrowError('Error');
+    });
+  });
 
-//   describe('register User', () => {
-//     const newUser = {
-//       userName: 'Pahasara',
-//       userEmail: 'pahasara@gmail.com',
-//       userPassword: '12345678',
-//       role: 'Admin',
-//     };
+  describe('register User', () => {
+    const newUser = {
+      userName: 'Pahasara',
+      userEmail: 'pahasara@gmail.com',
+      userPassword: '12345678',
+      role: 'Admin',
+    };
 
-//     test('should create user successfully', async () => {
-//       userRepo.insert = jest.fn().mockResolvedValue(newUser);
-//       const data = await registerUser(newUser);
-//       expect(data).toEqual(newUser);
-//       expect(sendNotification).toHaveBeenCalledWith(
-//         'Success',
-//         'User Registerd..!',
-//       );
-//     });
+    test('should create user successfully', async () => {
+      userRepo.insert = jest.fn().mockResolvedValue(newUser);
+      const data = await registerUser(newUser);
+      expect(data).toEqual(newUser);
+      expect(sendNotification).toHaveBeenCalledWith(
+        'Success',
+        'User Registerd..!',
+      );
+    });
 
-//     test('should handle failure to create user', async () => {
-//       userRepo.insert = jest.fn().mockRejectedValue(new Error('Error'));
-//       await expect(registerUser(newUser)).rejects.toThrowError('Error');
-//     });
-//   });
+    test('should handle failure to create user', async () => {
+      userRepo.insert = jest.fn().mockRejectedValue(new Error('Error'));
+      await expect(registerUser(newUser)).rejects.toThrowError('Error');
+    });
+  });
 
-//   describe('Update user', () => {
-//     const updatedUser: User = {
-//       userName: 'Pahasara',
-//       userEmail: 'pahasara@gmail.com',
-//       userPassword: '12345678',
-//       role: 'Admin',
-//     };
+  describe('Update user', () => {
+    const updatedUser: User = {
+      userName: 'Pahasara',
+      userEmail: 'pahasara@gmail.com',
+      userPassword: '12345678',
+      role: 'Admin',
+    };
 
-//     test('should update user successfully', async () => {
-//       const mockUpdateResult: UpdateResult = {
-//         affected: 1,
-//         raw: updatedUser,
-//         generatedMaps: [],
-//       };
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         update: jest.fn().mockResolvedValue(mockUpdateResult),
-//       });
+    test('should update user successfully', async () => {
+      const mockUpdateResult: UpdateResult = {
+        affected: 1,
+        raw: updatedUser,
+        generatedMaps: [],
+      };
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        update: jest.fn().mockResolvedValue(mockUpdateResult),
+      });
 
-//       const result = await updateUser('pahasara@gmail.com', updatedUser);
-//       expect(result).toEqual(mockUpdateResult);
-//       expect(sendNotification).toHaveBeenCalledWith(
-//         'Success',
-//         'User Updated..!',
-//       );
-//     });
+      const result = await updateUser('pahasara@gmail.com', updatedUser);
+      expect(result).toEqual(mockUpdateResult);
+      expect(sendNotification).toHaveBeenCalledWith(
+        'Success',
+        'User Updated..!',
+      );
+    });
 
-//     test('should handle failure to update user', async () => {
-//       const errorMessage = 'Error updating user.';
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         update: jest.fn().mockRejectedValue(new Error(errorMessage)),
-//       });
+    test('should handle failure to update user', async () => {
+      const errorMessage = 'Error updating user.';
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        update: jest.fn().mockRejectedValue(new Error(errorMessage)),
+      });
 
-//       await expect(
-//         updateUser('pahasara@gmail.com', updatedUser),
-//       ).rejects.toThrowError(errorMessage);
-//     });
-//   });
+      await expect(
+        updateUser('pahasara@gmail.com', updatedUser),
+      ).rejects.toThrowError(errorMessage);
+    });
+  });
 
-//   describe('Delete user', () => {
-//     const email = 'pahasara@gmail.com';
+  describe('Delete user', () => {
+    const email = 'pahasara@gmail.com';
 
-//     test('should delete user successfully', async () => {
-//       const mockDeleteResult: DeleteResult = {
-//         affected: 1,
-//         raw: undefined,
-//       }; // Mimic successful delete
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         delete: jest.fn().mockResolvedValue(mockDeleteResult),
-//       });
+    test('should delete user successfully', async () => {
+      const mockDeleteResult: DeleteResult = {
+        affected: 1,
+        raw: undefined,
+      }; // Mimic successful delete
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        delete: jest.fn().mockResolvedValue(mockDeleteResult),
+      });
 
-//       const result = await deleteUser(email);
-//       expect(result).toEqual(mockDeleteResult);
-//       expect(sendNotification).toHaveBeenCalledWith(
-//         'Success',
-//         'User Deleted..!',
-//       );
-//     });
+      const result = await deleteUser(email);
+      expect(result).toEqual(mockDeleteResult);
+      expect(sendNotification).toHaveBeenCalledWith(
+        'Success',
+        'User Deleted..!',
+      );
+    });
 
-//     test('should handle failure to delete user', async () => {
-//       const errorMessage = 'Error deleting user.';
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         delete: jest.fn().mockRejectedValue(new Error(errorMessage)),
-//       });
+    test('should handle failure to delete user', async () => {
+      const errorMessage = 'Error deleting user.';
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        delete: jest.fn().mockRejectedValue(new Error(errorMessage)),
+      });
 
-//       await expect(deleteUser(email)).rejects.toThrowError(errorMessage);
-//     });
-//   });
+      await expect(deleteUser(email)).rejects.toThrowError(errorMessage);
+    });
+  });
 
-//   describe('SignIn user', () => {
-//     const email = 'pahasara@gmail.com';
-//     const password = '12345678';
-//     const user = {
-//       userName: 'Pahasara',
-//       userEmail: 'pahasara@gmail.com',
-//       userPassword: '12345678',
-//       role: 'Admin',
-//     };
+  describe('SignIn user', () => {
+    const email = 'pahasara@gmail.com';
+    const password = '12345678';
+    const user = {
+      userName: 'Pahasara',
+      userEmail: 'pahasara@gmail.com',
+      userPassword: '12345678',
+      role: 'Admin',
+    };
   
-//     test('should signIn user successfully', async () => {
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         findOne: jest.fn().mockResolvedValue(user),
-//       }); // Mocking getRepository method
+    test('should signIn user successfully', async () => {
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        findOne: jest.fn().mockResolvedValue(user),
+      }); // Mocking getRepository method
   
-//       const data = await signInUser(email, password);
-//       expect(data).toEqual(user);
-//     });
+      const data = await signInUser(email, password);
+      expect(data).toEqual(user);
+    });
   
-//     test('should handle failure to signIn user', async () => {
-//       const errorMessage = 'Error';
-//       dataSource.manager.getRepository = jest.fn().mockReturnValue({
-//         findOne: jest.fn().mockRejectedValue(new Error(errorMessage)),
-//       }); // Mocking getRepository method
+    test('should handle failure to signIn user', async () => {
+      const errorMessage = 'Error';
+      dataSource.manager.getRepository = jest.fn().mockReturnValue({
+        findOne: jest.fn().mockRejectedValue(new Error(errorMessage)),
+      }); // Mocking getRepository method
   
-//       await expect(signInUser(email, password)).rejects.toThrowError(errorMessage);
-//     });
-//   });  
-// });
+      await expect(signInUser(email, password)).rejects.toThrowError(errorMessage);
+    });
+  });  
+});
