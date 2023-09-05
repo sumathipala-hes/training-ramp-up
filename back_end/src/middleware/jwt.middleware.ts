e76436cd3461e69edb471b7d2e1294ef36a6d938
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-09-05 23:50:18 +0530

    Refactored code

--- back_end/src/middleware/jwt.middleware.ts
+++ back_end/src/middleware/jwt.middleware.ts
@@ -1,22 +1,61 @@
 import jwt from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
 import { jwtConfig } from '../configs/jwt.config';
-import { User } from '../models/user.models';
 
 export const authorizationPermissions = (
   req: Request,
   res: Response,
   next: NextFunction,
 ): void => {
-  const accessToken = req.cookies.accessToken;
+  const accessToken = req.headers.authorization?.split(':')[1];
+
   if (!accessToken) {
     res.sendStatus(403);
   } else {
     try {
       const payload = jwt.verify(accessToken, jwtConfig.secretKey);
-      payload ? next() : res.sendStatus(401);
-    } catch {
+
+      if (payload) {
+        next();
+      } else {
+        res.sendStatus(401);
+      }
+    } catch (error) {
+      console.error('Authorization error:', error);
       res.sendStatus(403);
     }
+  }
+};
+
+export const authPermissions = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const roleType = req.headers.authorization?.split(':')[1];
+  console.log(roleType);
+
+  if (!roleType) {
+    return res.sendStatus(403);
+  }
+
+  try {
+    if (req.method === 'GET') {
+      return next();
+    }
+
+    const payload = jwt.verify(roleType, jwtConfig.userKey);
+
+    const userType = payload as { roleType: string };
+    const role = userType.roleType;
+    console.log(role);
+    if (role === 'ADMIN') {
+      return next();
+    } else {
+      return res.sendStatus(401);
+    }
+  } catch (error) {
+    console.error('Authentication error:', error);
+    return res.sendStatus(403);
   }
 };
--- back_end/src/routes/user.routes.ts
+++ back_end/src/routes/user.routes.ts
@@ -10,15 +10,18 @@ import {
   signOut,
 } from '../controllers/user.controller';
 import { validateUsers } from '../middleware/validate.users.middleware';
-import { authorizationPermissions } from '../middleware/jwt.middleware';
+import {
+  authPermissions,
+  authorizationPermissions,
+} from '../middleware/jwt.middleware';
 
 const router = express.Router();
 
 router.get('/', requestGetAllUsers);
 router.get('/:search', requestUsersByOne);
-router.post('/', validateUsers, requestCreateUser);
-router.put('/:id', validateUsers, requestUpdateUser);
-router.delete('/:id', requestDeleteUser);
+router.post('/', authPermissions, validateUsers, requestCreateUser);
+router.put('/:id', authPermissions, validateUsers, requestUpdateUser);
+router.delete('/:id', authPermissions, requestDeleteUser);
 
 router.post('/signIn', signIn);
 router.delete('/signOut', authorizationPermissions, signOut);
