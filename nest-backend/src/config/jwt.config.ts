afc0d4307848146804167b130f791c61e654e9dd
Sandun Induranga <sandutharu40@gmail.com>
2023-09-07 00:14:42 +0530

    Sign In Function

---
+++ nest-backend/src/config/jwt.config.ts
@@ -0,0 +1,9 @@
+import { config } from 'dotenv';
+config();
+
+export const jwtConfig = {
+  secretKey: process.env.SECRET_KEY!,
+  refreshKey: process.env.REFRESH_KEY!,
+  userKey: process.env.USER_KEY!,
+  expiresIn: process.env.EXPIRES_IN!,
+};
--- nest-backend/src/users/users.service.ts
+++ nest-backend/src/users/users.service.ts
@@ -4,20 +4,24 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { encrypt } from 'src/utils/password.util';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConfig } from 'src/config/jwt.config';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private studentRepository: Repository<User>,
+    private userRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
   create(createUserDto: CreateUserDto) {
-    return this.studentRepository.insert(createUserDto as User);
+    return this.userRepository.insert(createUserDto as User);
   }
 
   findAll() {
-    const users = this.studentRepository.find({ order: { email: 'DESC' } });
+    const users = this.userRepository.find({ order: { email: 'DESC' } });
     if (!users) {
       throw new Error('No Users Found');
     }
@@ -25,10 +29,42 @@ export class UsersService {
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
-    return this.studentRepository.update(id, updateUserDto as User);
+    return this.userRepository.update(id, updateUserDto as User);
   }
 
   remove(id: number) {
-    return this.studentRepository.delete(id);
+    return this.userRepository.delete(id);
+  }
+
+  async signInUser(email: string, password: string): Promise<any> {
+    try {
+      const user = await this.userRepository.findOne({
+        where: {
+          email: email,
+        },
+      });
+      password = encrypt(password);
+
+      if (user) {
+        if (user.password == password) {
+          const accessToken = this.jwtService.sign(
+            { email: user.email, role: user.role },
+            { secret: jwtConfig.secretKey!, expiresIn: '5h' },
+          );
+          const refreshToken = this.jwtService.sign(
+            { email: user.email },
+            { secret: jwtConfig.secretKey, expiresIn: '7d' },
+          );
+
+          return { accessToken: accessToken, refreshToken: refreshToken };
+        } else {
+          throw new Error('Password not match');
+        }
+      } else {
+        throw new Error('User not found');
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 }
