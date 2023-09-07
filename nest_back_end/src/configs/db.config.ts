74b7141b12a4d99d8eb8446c76991135a4f580cf
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-09-07 15:38:38 +0530

    added configs package and implement datasource

---
+++ nest_back_end/src/configs/db.config.ts
@@ -0,0 +1,17 @@
+import { config } from 'dotenv';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Student } from 'src/student/entities/student.entity';
+import { User } from 'src/user/entities/user.entity';
+config();
+
+export const dataSource: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Student, User],
+  synchronize: true,
+  logging: false,
+};
