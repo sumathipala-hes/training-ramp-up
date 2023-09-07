4102511480a20761eb9b3a9fb33b0966baf94fb5
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-09-07 16:12:43 +0530

    implement student model using @nestjs/class-validator

--- nest_back_end/src/student/dto/create-student.dto.ts
+++ nest_back_end/src/student/dto/create-student.dto.ts
@@ -1,1 +1,27 @@
-export class CreateStudentDto {}
+import { IsNotEmpty, IsNumber, IsString, IsDate } from "@nestjs/class-validator";
+
+export class CreateStudentDto {
+  @IsNotEmpty()
+  @IsNumber()
+  studentId: number;
+
+  @IsNotEmpty()
+  @IsString()
+  studentName: string;
+
+  @IsNotEmpty()
+  @IsString()
+  studentAddress: string;
+
+  @IsNotEmpty()
+  @IsString()
+  studentMobile: string;
+
+  @IsNotEmpty()
+  @IsDate()
+  studentDob: Date;
+
+  @IsNotEmpty()
+  @IsString()
+  studentGender: string;
+}
