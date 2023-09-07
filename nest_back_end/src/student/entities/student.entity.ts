bc10e92d77e4417837ec556adc2456e896784c59
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-09-07 16:11:47 +0530

    implement student entity using typeorm

--- nest_back_end/src/student/entities/student.entity.ts
+++ nest_back_end/src/student/entities/student.entity.ts
@@ -1,1 +1,22 @@
-export class Student {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Student {
+    @PrimaryGeneratedColumn()
+    studentId!: number;
+  
+    @Column()
+    studentName!: string;
+  
+    @Column()
+    studentAddress!: string;
+  
+    @Column()
+    studentMobile!: string;
+  
+    @Column()
+    studentDob!: Date;
+  
+    @Column()
+    studentGender!: string;
+}
