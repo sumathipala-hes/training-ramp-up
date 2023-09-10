e8e6e4e05d977f519f4f099def1b1b7526c6d370
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-09-10 13:25:23 +0530

    refactored user entity

--- nest_back_end/src/user/entities/user.entity.ts
+++ nest_back_end/src/user/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Entity, Column, BaseEntity, PrimaryColumn } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @Column()
+  userName: string;
+
+  @PrimaryColumn({ unique: true })
+  userEmail: string;
+
+  @Column()
+  userPassword: string;
+
+  @Column()
+  role: string;
+}
