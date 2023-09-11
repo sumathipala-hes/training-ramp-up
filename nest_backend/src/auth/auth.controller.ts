74709942ec9af13fd44672366a5d7b7163bebc5b
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-09-11 11:49:33 +0530

    refactered auth

--- nest_backend/src/auth/auth.controller.ts
+++ nest_backend/src/auth/auth.controller.ts
@@ -1,4 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('users')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signIn')
+  async signIn(
+    @Body() signInDto: { email: string; password: string },
+    @Res() res: Response,
+  ) {
+    try {
+      const token = await this.authService.signInUser(
+        signInDto.email,
+        signInDto.password,
+      );
+
+      res.cookie('accessToken', token.accessToken, {
+        maxAge: 1000 * 60 * 5,
+        httpOnly: true,
+      });
+
+      res.cookie('refreshToken', token.refreshToken, {
+        maxAge: 60 * 60 * 24 * 1000,
+        httpOnly: true,
+      });
+
+      res.status(200).json({
+        accessToken: token.accessToken,
+        refreshToken: token.refreshToken,
+      });
+    } catch (error: any) {
+      if (error.message === 'User not found') {
+        return res.status(400).json({ message: 'User not found' });
+      } else {
+        return res.status(500).json({ message: 'An error occurred' });
+      }
+    }
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Delete('signOut')
+  async signOut(@Res({ passthrough: true }) res: Response) {
+    try {
+      res.clearCookie('accessToken');
+      res.clearCookie('refreshToken');
+      res.status(200).json({ message: 'Sign out successfully' });
+    } catch (error: any) {
+      res.status(500).json(error);
+    }
+  }
+}
--- nest_backend/src/auth/auth.service.ts
+++ nest_backend/src/auth/auth.service.ts
@@ -1,4 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from 'src/users/entities/user.entity';
+import { encrypt } from 'src/util/encrypted.decrypted.util';
+import { Repository } from 'typeorm/repository/Repository';
+import { jwtConfig } from '../config/jwt.config';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signInUser(
+    email: string,
+    password: string,
+  ): Promise<{ accessToken: string; refreshToken: string }> {
+    const user = await this.userRepository.findOne({ where: { email: email } });
+    password = encrypt(password);
+
+    if (user?.password !== password) {
+      throw new UnauthorizedException();
+    }
+    const payload = { email: user.email, roleType: user.roleType };
+    return {
+      accessToken: await this.jwtService.signAsync(payload, {
+        expiresIn: jwtConfig.accessExpiresIn,
+        secret: jwtConfig.secretKey,
+      }),
+      refreshToken: await this.jwtService.signAsync(payload, {
+        expiresIn: jwtConfig.refreshExpiresIn,
+        secret: jwtConfig.refreshKey,
+      }),
+    };
+  }
+}
