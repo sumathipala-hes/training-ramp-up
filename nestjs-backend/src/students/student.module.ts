7434ddbeb1c036ff614ac202d64261524e8fceaa
Pamudi <pamudiabeykoon4@gmail.com>
2023-09-13 00:05:37 +0530

    authorization for student apis

--- nestjs-backend/src/students/student.module.ts
+++ nestjs-backend/src/students/student.module.ts
@@ -1,13 +1,21 @@
 /* eslint-disable prettier/prettier */
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { StudentController } from "./student.controller";
 import { StudentService } from "./student.service";
 import { Student } from "./entities/student.entity";
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthMiddleware } from "src/middleware/auth.middleware";
+import { AdminAccess } from "src/middleware/admin.access.middleware";
+import { JwtService } from "@nestjs/jwt";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Student])],
     controllers: [StudentController],
-    providers: [StudentService],
+    providers: [StudentService, JwtService],
 })
-export class StudentModule {}
+
+export class StudentModule implements NestModule{
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(AuthMiddleware, AdminAccess).forRoutes('/students');
+    }
+}
