5566e6f54bc9d3d82db2fb6670d45949d12db1a2
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-09-12 12:55:54 +0530

    Add student service unit test

--- nest_backend/src/users/users.service.spec.ts
+++ nest_backend/src/users/users.service.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { InsertResult, Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { decrypt, encrypt } from 'src/util/encrypted.decrypted.util';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
+
+  const expectedResult: User[] = [
+    {
+      roleType: 'ADMIN',
+      name: 'Nimesh',
+      address: 'Galle',
+      email: 'nimesh123@gmail.com',
+      mobileNumber: '0761234567',
+      dob: new Date('2001 - 12 - 15'),
+      gender: 'Male',
+      password: 'Nimesh12@345',
+    },
+    {
+      roleType: 'USER',
+      name: 'Nimesh',
+      address: 'Galle',
+      email: 'nimesh12@gmail.com',
+      mobileNumber: '0761234567',
+      dob: new Date('2001 - 12 - 15'),
+      gender: 'Male',
+      password: 'Nimesh12@345',
+    },
+  ];
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+
 });
