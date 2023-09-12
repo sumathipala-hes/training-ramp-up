b05081e92d3484956196c26d311302b6a47a9983
exagenio <123676288+exagenio@users.noreply.github.com>
2023-09-12 09:28:47 +0530

    initialize backend with nestjs

--- back-end/src/services/repositories/userRepo.ts
+++
@@ -1,6 +0,0 @@
-import AppDataSource from "../../config/dataSoure";
-import { User } from "../../models/user";
-
-//create user repo
-const userRepository = AppDataSource.getRepository(User);
-export default userRepository;
