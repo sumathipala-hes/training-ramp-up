2963ae3cf2e71ccd7ee212c54b9aed2153daf373
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-09-13 18:59:32 +0530

    implement authentication

--- nest_back_end/src/auth/auth.controller.ts
+++ nest_back_end/src/auth/auth.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { Response } from 'express';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/v1/user')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('signIn')
+  async signIn(@Body() createUserDto: CreateUserDto, @Res() res: Response) {
+    try {
+      console.log(createUserDto);
+
+      const user = await this.authService.signIn(createUserDto);
+
+      res.cookie('refreshToken', user.refreshToken, {
+        httpOnly: true,
+        secure: true,
+      });
+      res.cookie('accessToken', user.accessToken, {
+        httpOnly: true,
+        secure: true,
+      });
+      console.log(user.accessToken, user.refreshToken);
+
+      return res.status(200).json({
+        accessToken: user.accessToken,
+        refreshToken: user.refreshToken,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+  }
+}
--- nest_back_end/src/auth/auth.module.ts
+++ nest_back_end/src/auth/auth.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './auth.constants';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
