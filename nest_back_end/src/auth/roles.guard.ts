43eec8f4803fdf5d46c14ab7650222e659ff13ee
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-09-13 23:55:44 +0530

    implement roles.guard

---
+++ nest_back_end/src/auth/roles.guard.ts
@@ -0,0 +1,46 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'src/enums/role.enum';
+import { ROLES_KEY } from './roles.decorator';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (!requiredRoles) {
+      return true;
+    }
+    console.log('requiredRoles :' + requiredRoles);
+
+    const request = context.switchToHttp().getRequest();
+    const accessToken = request.headers.cookie?.split('=')[1];
+
+    console.log('accessToken :' + accessToken);
+
+    if (!accessToken) {
+      throw new ForbiddenException('Access token not found');
+    }
+
+    try {
+      if (accessToken == Role.Admin) {
+        return true;
+      }
+      throw new ForbiddenException('Forbidden');
+    } catch (error) {
+      throw new UnauthorizedException('Invalid accessToken');
+    }
+  }
+}
