1437eab905064dae64341187a83bdbf36e52cfd5
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-09-14 23:18:59 +0530

    Refactored code

--- nest_backend/src/students/students.controller.spec.ts
+++ nest_backend/src/students/students.controller.spec.ts
@@ -5,6 +5,7 @@ import { JwtService } from '@nestjs/jwt';
 import { CreateStudentDto } from './dto/create-student.dto';
 import { UpdateStudentDto } from './dto/update-student.dto';
 import { DeleteResult, UpdateResult } from 'typeorm';
+import { StudentResponseData } from './dto/response-data';
 
 describe('StudentsController', () => {
   let studentsController: StudentsController;
@@ -65,6 +66,26 @@ describe('StudentsController', () => {
       );
       expect(mockInsertResult).toEqual(mockInsertResult);
     });
+
+    it('should fail in student creation and throw an error', async () => {
+      jest
+        .spyOn(studentsService, 'createStudent')
+        .mockRejectedValue(new Error());
+
+      await expect(
+        studentsController.create(createStudentDto),
+      ).rejects.toThrowError();
+    });
+
+    it('should fail and throw an error if student already exists', async () => {
+      jest
+        .spyOn(studentsService, 'createStudent')
+        .mockRejectedValue(new Error('Student already exists'));
+
+      await expect(studentsController.create(createStudentDto)).rejects.toThrow(
+        'Student already exists',
+      );
+    });
   });
 
   describe('updateStudent', () => {
@@ -97,6 +118,26 @@ describe('StudentsController', () => {
       );
       expect(updateStudentResult).toEqual(updateStudentResult);
     });
+
+    it('should fail in student update and throw an error', async () => {
+      jest
+        .spyOn(studentsService, 'updateStudent')
+        .mockRejectedValue(new Error());
+
+      await expect(
+        studentsController.update(id, updateStudentDto),
+      ).rejects.toThrowError();
+    });
+
+    it('should fail and throw an error if student does not exists', async () => {
+      jest
+        .spyOn(studentsService, 'updateStudent')
+        .mockRejectedValue(new Error('Student does not exists'));
+
+      await expect(
+        studentsController.update(id, updateStudentDto),
+      ).rejects.toThrowError('Student does not exists');
+    });
   });
 
   describe('removeStudent', () => {
@@ -115,6 +156,105 @@ describe('StudentsController', () => {
 
       expect(studentsService.removeStudent).toHaveBeenCalledWith(id);
       expect(mockDeleteResult).toEqual(mockDeleteResult);
+    });
+
+    it('should fail in student deletion and throw an error', async () => {
+      jest
+        .spyOn(studentsService, 'removeStudent')
+        .mockRejectedValue(new Error());
+
+      await expect(studentsController.remove(id)).rejects.toThrowError();
+    });
+
+    it('should fail and throw an error if student does not exists', async () => {
+      jest
+        .spyOn(studentsService, 'removeStudent')
+        .mockRejectedValue(new Error('Student does not exists'));
+
+      await expect(studentsController.remove(id)).rejects.toThrowError(
+        'Student does not exists',
+      );
+    });
+  });
+
+  describe('findAllStudents', () => {
+    it('should return an array of students', async () => {
+      const result: StudentResponseData = {
+        message: 'Student found successfully',
+        data: [
+          {
+            id: 1,
+            name: 'Nimesh',
+            address: 'Galle',
+            mobileNumber: '0761234567',
+            dob: new Date('2001 - 12 - 15'),
+            gender: 'Male',
+          },
+        ],
+      };
+
+      jest.spyOn(studentsService, 'findAllStudents').mockResolvedValue(result);
+
+      expect(await studentsController.findAll()).toBe(result);
+    });
+
+    it('should fail in students retrieval and throw an error', async () => {
+      jest
+        .spyOn(studentsService, 'findAllStudents')
+        .mockRejectedValue(new Error());
+      await expect(studentsController.findAll()).rejects.toThrowError();
+    });
+
+    it('should return an empty array if no students found', async () => {
+      const result: StudentResponseData = {
+        message: 'Student not found',
+        data: [],
+      };
+
+      jest.spyOn(studentsService, 'findAllStudents').mockResolvedValue(result);
+
+      expect(await studentsController.findAll()).toBe(result);
+    });
+  });
+
+  describe('findOneStudent', () => {
+    const search = 'Nimesh';
+    it('should return a student', async () => {
+      const result: StudentResponseData = {
+        message: 'Student found successfully',
+        data: [
+          {
+            id: 1,
+            name: 'Nimesh',
+            address: 'Galle',
+            mobileNumber: '0761234567',
+            dob: new Date('2001 - 12 - 15'),
+            gender: 'Male',
+          },
+        ],
+      };
+
+      jest.spyOn(studentsService, 'findOneStudent').mockResolvedValue(result);
+
+      expect(await studentsController.findOne(search)).toBe(result);
+    });
+
+    it('should fail in student retrieval and throw an error', async () => {
+      jest
+        .spyOn(studentsService, 'findOneStudent')
+        .mockRejectedValue(new Error());
+      await expect(studentsController.findOne(search)).rejects.toThrowError();
+    });
+
+    it('should return an empty array if no students found', async () => {
+      const result: StudentResponseData = {
+        message: 'Student not found',
+        data: [],
+      };
+
+      jest.spyOn(studentsService, 'findOneStudent').mockResolvedValue(result);
+
+      expect(await studentsController.findOne(search)).toBe(result);
     });
   });
 });
--- nest_backend/src/users/users.controller.spec.ts
+++ nest_backend/src/users/users.controller.spec.ts
@@ -5,6 +5,7 @@ import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from './dto/create-user.dto';
 import { DeleteResult, InsertResult, UpdateResult } from 'typeorm';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UserResponseData } from './dto/response-data';
 
 describe('UsersController', () => {
   let usersService: UsersService;
@@ -37,6 +38,87 @@ describe('UsersController', () => {
     expect(controller).toBeDefined();
   });
 
+  describe('findAllUsers', () => {
+    it('should return an array of users', async () => {
+      const result: UserResponseData = {
+        message: 'User found successfully',
+        data: [
+          {
+            roleType: 'ADMIN',
+            name: 'Nimesh',
+            address: 'Galle',
+            email: 'nimesh12@gmail.com',
+            mobileNumber: '0761234567',
+            dob: new Date('2001 - 12 - 15'),
+            gender: 'Male',
+            password: 'Nimesh12@345',
+          },
+        ],
+      };
+
+      jest.spyOn(usersService, 'findAllUsers').mockResolvedValue(result);
+
+      expect(await controller.findAll()).toBe(result);
+    });
+
+    it('should fail in student retrieval and throw an error', async () => {
+      jest.spyOn(usersService, 'findAllUsers').mockRejectedValue(new Error());
+      await expect(controller.findAll()).rejects.toThrowError();
+    });
+
+    it('should return an empty array if no users found', async () => {
+      const result: UserResponseData = {
+        message: 'User not found',
+        data: [],
+      };
+
+      jest.spyOn(usersService, 'findAllUsers').mockResolvedValue(result);
+
+      expect(await controller.findAll()).toBe(result);
+    });
+  });
+
+  describe('findOneUser', () => {
+    const search = 'Nimesh';
+    it('should return a user', async () => {
+      const result: UserResponseData = {
+        message: 'User found successfully',
+        data: [
+          {
+            roleType: 'ADMIN',
+            name: 'Nimesh',
+            address: 'Galle',
+            email: 'nimesh12@gmail.com',
+            mobileNumber: '0761234567',
+            dob: new Date('2001 - 12 - 15'),
+            gender: 'Male',
+            password: 'Nimesh12@345',
+          },
+        ],
+      };
+
+      jest.spyOn(usersService, 'findOneUser').mockResolvedValue(result);
+
+      expect(await controller.findOne(search)).toBe(result);
+    });
+
+    it('should fail in student retrieval and throw an error', async () => {
+      jest.spyOn(usersService, 'findOneUser').mockRejectedValue(new Error());
+      await expect(controller.findOne(search)).rejects.toThrowError();
+    });
+
+    it('should return an empty array if no users found', async () => {
+      const result: UserResponseData = {
+        message: 'User not found',
+        data: [],
+      };
+
+      jest.spyOn(usersService, 'findOneUser').mockResolvedValue(result);
+
+      expect(await controller.findOne(search)).toBe(result);
+    });
+  });
+
   describe('createUser', () => {
     const createUserDto: CreateUserDto = {
       roleType: 'ADMIN',
@@ -62,6 +144,21 @@ describe('UsersController', () => {
 
       expect(usersService.createUser).toHaveBeenCalledWith(createUserDto);
       expect(usersService.createUser).toHaveBeenCalledTimes(1);
+    });
+
+    it('should fail and throw an error if email already exists', async () => {
+      jest
+        .spyOn(usersService, 'createUser')
+        .mockRejectedValue(new Error('Email already exists'));
+
+      await expect(controller.create(createUserDto)).rejects.toThrow(
+        'Email already exists',
+      );
+    });
+
+    it('should fail in student creation and throw an error', async () => {
+      jest.spyOn(usersService, 'createUser').mockRejectedValue(new Error());
+      await expect(controller.create(createUserDto)).rejects.toThrow();
     });
   });
 
@@ -94,6 +191,23 @@ describe('UsersController', () => {
       expect(usersService.updateUser).toHaveBeenCalledWith(email, updatedUser);
       expect(updateUserResult).toEqual(updateUserResult);
     });
+
+    it('should fail in student update and throw an error', async () => {
+      jest.spyOn(usersService, 'updateUser').mockRejectedValue(new Error());
+      await expect(
+        controller.update(email, updatedUser),
+      ).rejects.toThrowError();
+    });
+
+    it('should fail and throw an error if email does not exists', async () => {
+      jest
+        .spyOn(usersService, 'updateUser')
+        .mockRejectedValue(new Error('Email does not exists'));
+
+      await expect(controller.update(email, updatedUser)).rejects.toThrowError(
+        'Email does not exists',
+      );
+    });
   });
 
   describe('removeUser', () => {
@@ -112,6 +226,21 @@ describe('UsersController', () => {
 
       expect(usersService.removeUser).toHaveBeenCalledWith(email);
       expect(mockDeleteResult).toEqual(mockDeleteResult);
+    });
+
+    it('should fail in student deletion and throw an error', async () => {
+      jest.spyOn(usersService, 'removeUser').mockRejectedValue(new Error());
+      await expect(controller.remove(email)).rejects.toThrowError();
+    });
+
+    it('should fail and throw an error if email does not exists', async () => {
+      jest
+        .spyOn(usersService, 'removeUser')
+        .mockRejectedValue(new Error('Email does not exists'));
+
+      await expect(controller.remove(email)).rejects.toThrowError(
+        'Email does not exists',
+      );
     });
   });
 });
