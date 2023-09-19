5bb394cd7657b9bf550f1a78aefe09159d2ba2a1
Sandun Induranga <sandutharu40@gmail.com>
2023-09-19 19:16:29 +0530

    Content Updated

---
+++ nest-backend/src/config/firebase.config.ts
@@ -0,0 +1,17 @@
+import * as admin from 'firebase-admin';
+
+export const serviceAccount: admin.ServiceAccount = {
+  type: 'service_account',
+  project_id: 'ramp-up-f38d6',
+  private_key_id: '1998a25e77564775eb136a800c637b2971d10dec',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDezQ3BF80nCN7t\n9GetXsxV0yTgGEUV9cAJCTTHEfCNRmFMhCmDKNHqso9AZqwTYffXrQheFRO8581i\nQ8qPpXB/zG5SAOtuEGrVn77dFkG7OvIo9nnQx1/4x6Jg6SE0DFXSpGPRvP8w7s+J\nC4EPYebin1p9LSJH52/n+BpnST31tVSwk4lKoGziYBzM6RURHfyOwRaH5IqUsyHn\ncN+kCFDQVGY+n8I2Lj0ZNzglbTIjoXjYz2rMqVyg11GXaVjOGPFFoAxkwAaFz0T9\nGIvMJihlMRkn7O//OivHTRTbDfUCu1V00lCtWRghebaLj/UYklQE5PfKS5lY8mDV\nmyo3/ll7AgMBAAECggEAKwWvhTzCWapF2e8a4ghvLpdyBG/LT10/8r67WUFmkPEp\nMqxGk9QbDNXkAYyR58BAKn54IiqxwAWGzFXI1Ioeuh7QorcYvw0ehBevtGNq1+ue\n/Tfy99S3/IMYINyDk9hZ7i3eNI7pPBqxKoZ1KnI+Mj6RK6C+gOH44vMurgkflMVN\nvRgmjL4xY2c+QzrnYdRuTw0EnaLZccgg4POuG+gR0IAROqwvYznYmm2VjzAEKq2N\nLiKZeVgltWCRbkh20XKxgHovRBv7Yptgicd9MMWN/kSULEEmqt4CkHOOqjfiqIu+\n1exPPtGO99iHye9+2AV5fDyk3PqLSIbvvWFxzwKcGQKBgQD5qAFOAu6kIZAK+Yv7\n7YhXo48WvsnN6/MzbyeMKNqf526st4ndbGlt1RpGFDMMzG74lup+BafucfVqQPt+\no1zo2tniHa8ayyQymjhSITqqnpxWj3ZYcPQNnzYrC+pW/7O22gUh0oc01cPT17rP\nID1e3S1WFz8LpZkrvLInDL2FtwKBgQDkdltq8S335TE3gMO/sOjDVWl5aQbZnVQy\nqSYV7qSVXcipMBJP2k+/OAMiKBg8EGvQJhA5JMFSuLfkPt1AjGknsBsSdkF19pyv\nmqWplx7l+s0cEo9Vd3tu4cARMylDf3N+cnD+ztP0n7t55+/qfGGhjcqA4AfLW4yq\nPOoUqHFqXQKBgQDA1dk0JaWo11fIclqC3EVs1CQJAJMl4HN4rgnRBBliW8Or2xS9\nGCkTMgBaUu3fm+S3VPKJJttAnXtfeBQKDCsOnUSgmCcRvgkWoPKR1aLX+S1u2t8h\njxWjk++mLkzyqA1rVlmSd2gCEn9xGPftVsywgoyrK8eBuoZmNxkwenHFXwKBgFew\nwkgbCJzlAO4f44K5Ya5QI2rxkCqJA99qI285qTyXwcSG6hByaDeqRTGgGCU93FQJ\nuCJm5DlfI15dbsku29Ha+5krgNbJcLzKbBnbIoX9rSrASPnyIZOe3Pz9GTKKQgdf\n39U+CAcWJHyXNPRZB5Rd+0NaOkOpASX6uZJyUQ4VAoGATKqO3FQd4m+0TiiyHy5r\nhFdvK3UIi3YxaRVDDTxF+zYf9gIX5ulE44vlKOZ9le9t8/gf+tQJXEqgCNbHMKTJ\n/H8CZk36Dz0mKKmIs7wBqXhimAk5wVqxxT2A1PVZlGW0VzDN3T5AVTqwLdHrNZeQ\nAV9KZL9omFLotQdKH4XmN3M=\n-----END PRIVATE KEY-----\n',
+  client_email: 'firebase-adminsdk-uz9ck@ramp-up-f38d6.iam.gserviceaccount.com',
+  client_id: '113191716367552970570',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-uz9ck%40ramp-up-f38d6.iam.gserviceaccount.com',
+  universe_domain: 'googleapis.com',
+} as admin.ServiceAccount;
