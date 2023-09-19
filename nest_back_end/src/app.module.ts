173040c8dbd6517e893bd796e48117dddd24fb25
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-09-19 20:56:15 +0530

    Implemented and fullyRefactored firebase push notifications

--- nest_back_end/src/app.module.ts
+++ nest_back_end/src/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { dataSource } from './configs/db.config';
+import { dataSource } from './config/config.db';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRoot(dataSource),
     UserModule,
     AuthModule,
+    ConfigModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- nest_back_end/src/main.ts
+++ nest_back_end/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as admin from 'firebase-admin';
+import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const configService = app.get(ConfigService);
+
+  // Initialize the Firebase Admin SDK using your serviceAccount credentials
+  admin.initializeApp({
+    credential: admin.credential.cert(configService.getFirebaseAdminConfig()),
+  });
+
   await app.listen(4000);
 }
 bootstrap();
