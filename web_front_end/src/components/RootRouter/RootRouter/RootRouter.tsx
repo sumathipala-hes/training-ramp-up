4afe80ac9f845de71b1a6aa804c272bfc49101e1
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-09 23:39:38 +0530

    implement Home Container and implement Route Pard

--- web_front_end/src/components/RootRouter/RootRouter/RootRouter.tsx
+++ web_front_end/src/components/RootRouter/RootRouter/RootRouter.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { ROUTE_HOME } from "../../../util/routes";
+import Home from "../../../containers/Home/Home";
 
 export default function Content() {
   return (
     <Routes>
-      <Route path={ROUTE_HOME}  />
+      <Route path={ROUTE_HOME} element={<Home />}  />
     </Routes>
   );
 }
---
+++ web_front_end/src/containers/Home/Home.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+    return (
+        <div className="App">
+        <header className="App-header">
+            <p>
+            Home
+            </p>
+        </header>
+        </div>
+    );
+}
