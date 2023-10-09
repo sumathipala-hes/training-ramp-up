42198cf355fcab41cdf3f93c00e7c149c8bce548
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-09 19:09:51 +0530

    refactor some issues

--- web_front_end/.eslintrc.js
+++ web_front_end/.eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
-    "airbnb",
-    "plugin:prettier/recommended",
+    "prettier",
   ],
   overrides: [
     {
@@ -19,9 +20,11 @@ module.exports = {
       },
     },
   ],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  plugins: ["@typescript-eslint"],
   rules: {},
 };
