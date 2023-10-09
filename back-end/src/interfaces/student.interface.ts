168e38dcfdee9579cb93abe026799a7ce5cb8397
shamal-iroshan <shamal@fcodelabs.com>
2023-10-09 11:06:24 +0530

    reset main to initial

--- back-end/src/interfaces/student.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface StudentData {
-  id: number;
-  name: string;
-  gender: string;
-  address: string;
-  mobile: number;
-  dob: string;
-  age: number;
-}
--- front-end/src/utils/RoutePaths.tsx
+++
@@ -1,6 +0,0 @@
-export const routes = {
-    register : "/",
-    login : "/login",
-    admin : "/admin",
-    main : "/main"
-}
