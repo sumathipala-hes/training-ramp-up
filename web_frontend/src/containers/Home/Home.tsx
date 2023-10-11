b701ec579fb20be74e33c97669936fbcd0b13fa5
Sandun Induranga <sandutharu40@gmail.com>
2023-10-11 12:23:22 +0530

    Package System Updated

--- web_frontend/src/containers/Home/Home.tsx
+++ web_frontend/src/containers/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box } from "@mui/material";
-import StudentDataGrid from "../../components/StudentDataGrid/StudentDataGrid";
+import StudentDataGrid from "./StudentDataGrid";
 
 const Home = () => {
   return (
