764001b733d899c817b7d84514a231779b3f8d54
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-10-13 12:30:47 +0530

    update project

--- web-front_end/src/Home/TableGrid/StudentDataGrid.tsx
+++ web-front_end/src/Home/TableGrid/StudentDataGrid.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-no-undef */
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
--- web-front_end/src/redux/studentSlice.ts
+++ web-front_end/src/redux/studentSlice.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { generateAge } from "../util/generateAgeUtil";
 
