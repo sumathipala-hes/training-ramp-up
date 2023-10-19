3327856d4a296c677d292daa188a053be75414b7
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-19 15:35:55 +0530

    refactored redux user slice

--- web_front_end/src/redux/user/userSlice.ts
+++ web_front_end/src/redux/user/userSlice.ts
@@ -19,19 +19,19 @@ export const userSlice = createSlice({
     name: "userDataList",
     initialState,
     reducers: {
-        addTableData(state, action: PayloadAction<IUser>) {
+        addUserData(state, action: PayloadAction<IUser>) {
             state.userEntries.unshift(action.payload);
         },
-        fetchTableData() {},
-        updateTableData(state, action: PayloadAction<IUser>) {
+        fetchUsersData() {},
+        updateUserData(state, action: PayloadAction<IUser>) {
             console.log(action.payload);
         },
-        removeTableData(state, action: PayloadAction<string>) {
+        removeUserData(state, action: PayloadAction<string>) {
             state.userEntries = state.userEntries.filter(
                 (item) => item.userEmail !== action.payload,
             );
         },
-        setTableData: (state, action: PayloadAction<IUser[]>) => {
+        setUserData: (state, action: PayloadAction<IUser[]>) => {
             state.userEntries = action.payload;
         },
     },
