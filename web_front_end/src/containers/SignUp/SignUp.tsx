c1206a3b21bef85413046445dbc6f9d80544f7e5
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-19 15:01:11 +0530

    implement SigUp page Ui

--- web_front_end/src/containers/SignUp/SignUp.tsx
+++ web_front_end/src/containers/SignUp/SignUp.tsx
@@ -1,5 +1,90 @@
-import React from 'react';
+import React from "react";
+import { Button, Card, Container, TextField, Typography } from "@mui/material";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
-export default function SignUp(){
-    return <div>SignUp</div>
+export default function SignUp() {
+    return (
+        <Container
+            maxWidth="sm"
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "100vh",
+            }}
+        >
+            <Card
+                variant="outlined"
+                sx={{
+                    margin: "25px",
+                    borderRadius: "30px",
+                    backgroundColor: "#ECF0F1",
+                    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+                    padding: "1rem",
+                    width: "60%",
+                }}
+            >
+                <Typography
+                    variant="h3"
+                    align="center"
+                    sx={{
+                        fontFamily: "Ubuntu, sans-serif",
+                        color: "#01ACDC",
+                        marginBottom: "30px",
+                        fontWeight: "600",
+                    }}
+                >
+                    Sign Up
+                </Typography>
+
+                <form>
+                    <TextField
+                        size="small"
+                        label="User Name"
+                        id="outlined-basic"
+                        variant="outlined"
+                        fullWidth
+                        required
+                        sx={{ mb: 2 }}
+                    />
+                    <TextField
+                        size="small"
+                        label="User Email"
+                        id="outlined-basic"
+                        variant="outlined"
+                        fullWidth
+                        required
+                        // margin="normal"
+                    />
+                    <TextField
+                        size="small"
+                        label="User Password"
+                        id="outlined-basic"
+                        variant="outlined"
+                        type="password"
+                        fullWidth
+                        required
+                        margin="normal"
+                    />
+                    <Button
+                        size="small"
+                        variant="contained"
+                        fullWidth
+                        sx={{
+                            borderRadius: "10px",
+                            mt: 1,
+                            backgroundColor: "#01ACDC",
+                            color: "#fff",
+                            "&:hover": { backgroundColor: "#01ACDC" },
+                            gap: "10px",
+                            fontFamily: "Ubuntu, sans-serif",
+                        }}
+                    >
+                        Sign Up
+                        <ArrowForwardIcon />
+                    </Button>
+                </form>
+            </Card>
+        </Container>
+    );
 }
