f6be58feb299b1fa7e40bf0b5162ba27c051ffe0
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-19 09:18:59 +0530

    implement SignIn Page route part

--- web_front_end/src/components/RootRouter/RootRouter/RootRouter.tsx
+++ web_front_end/src/components/RootRouter/RootRouter/RootRouter.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { ROUTE_HOME } from "../../../util/routes";
+import { ROUTE_HOME, ROUTE_SIGNIN } from "../../../util/routes";
 import Home from "../../../containers/Home/Home";
+import SignIn from "../../../containers/SignIn/SignIn";
 
 export default function Content() {
   return (
     <Routes>
       <Route path={ROUTE_HOME} element={<Home />}  />
+      <Route path={ROUTE_SIGNIN} element={<SignIn />} />
     </Routes>
   );
 }
