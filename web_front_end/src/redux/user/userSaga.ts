35bf6a684fdd60bfe1c67d609502ce985b083504
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-19 15:46:26 +0530

    refactored redux user saga

---
+++ web_front_end/src/redux/user/userSaga.ts
@@ -0,0 +1,79 @@
+import { all, call, put, takeEvery } from "redux-saga/effects";
+import { PayloadAction } from "@reduxjs/toolkit";
+import { api } from "../../api/api";
+import { userDataActions } from "./userSlice";
+
+// Define data types
+interface IUserData {
+    userName: string;
+    userEmail: string;
+    userPassword: string;
+    role: string;
+}
+
+// Define response types
+interface IResponseData {
+    data: {
+        data: IUserData[];
+    };
+}
+
+// Define action types
+const { fetchUsersData, removeUserData, updateUserData, setUserData } =
+    userDataActions;
+
+// Define saga functions
+function* getAllUserDataRows() {
+    try {
+        const response: IResponseData = yield call(api.get, "/user");
+        console.log(response.data.data);
+        yield put(setUserData(response.data.data));
+    } catch (e) {
+        alert("Loading data failed. Please try again." + e);
+    }
+}
+
+function* updateUserDataRow(action: PayloadAction<IUserData>) {
+    const data = action.payload;
+    const isUpdate: boolean = data.userEmail !== '';
+
+    const userData: IUserData = {
+        userName: data.userName,
+        userEmail: data.userEmail,
+        userPassword: data.userPassword,
+        role: data.role,
+    };
+
+    try {
+        yield call(
+            isUpdate ? api.put : api.post,
+            `/student/${isUpdate ? data.userEmail : ""}`,
+            userData,
+        );
+        yield call(getAllUserDataRows);
+    } catch (e) {
+        alert("Saving or Updating data failed. Please try again." + e);
+    }
+}
+
+function* deleteUserDataRow(action: PayloadAction<string>) {
+    const userEmail = action.payload;
+    try {
+        yield call(api.delete, `/student/${userEmail}`);
+        yield call(getAllUserDataRows);
+    } catch (e) {
+        alert("Deleting data failed. Please try again." + e + userEmail);
+    }
+}
+
+function* tableUserSaga() {
+    yield takeEvery(fetchUsersData, getAllUserDataRows);
+    yield takeEvery(updateUserData, updateUserDataRow);
+    yield takeEvery(removeUserData, deleteUserDataRow);
+}
+
+function* mySaga() {
+    yield all([tableUserSaga()]);
+}
+
+export default mySaga;
