d80e2f85488de7cc25809dd6e56837faa17cde1b
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-10-23 21:34:17 +0530

    fullyRefactored Role Gard

--- web_front_end/src/api/api.ts
+++ web_front_end/src/api/api.ts
@@ -5,4 +5,36 @@ const api = axios.create({
   withCredentials: true,
 });
 
+api.interceptors.response.use(
+  config => {
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  },
+);
+
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  async error => {
+    if (error.response && error.response.status === 401) {
+      try {
+        const refreshedTokenResponse = await api.post("/user/refreshToken", {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        });
+        console.log("Token refreshed:", refreshedTokenResponse);
+        return refreshedTokenResponse;
+      } catch (refreshError) {
+        console.error("Token refresh failed:", refreshError);
+        return Promise.reject(refreshError);
+      }
+    } else {
+      return Promise.reject(error);
+    }
+  },
+);
+
 export { api };
