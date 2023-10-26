8e28956ab65c5569217b4a6ada6bff5bec154e26
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-10-26 10:04:56 +0530

    Resovle confict

---
+++ web-front_end/src/containers/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StudentDataGrid from "../Home/TableGrid/StudentDataGrid";
+
+const Home = () => {
+  return (
+    <>
+      <StudentDataGrid />
+    </>
+  );
+};
+
+export default Home;
