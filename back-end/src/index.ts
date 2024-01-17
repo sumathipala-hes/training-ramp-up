583669595388deca5464008e89731b3a1181b36f
Sudarshan Sumathipala <118509143+sumathipala-hes@users.noreply.github.com>
2024-01-17 23:17:47 +0530

    Initialized back end project
    Initialize a NodeJS project with Express and typescript.  [IN-256]
    Configure Eslint and Prettier. [IN-257]

---
+++ back-end/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express + TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
