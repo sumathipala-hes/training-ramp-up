b521703ce219f7fd8fa6c5e463153cc853b002dc
Sudarshan Sumathipala <118509143+sumathipala-hes@users.noreply.github.com>
2024-02-04 21:57:37 +0530

    Login UI created.

---
+++ front-end/src/pages/Lgin/Login.tsx
@@ -0,0 +1,209 @@
+import { VisibilityOffOutlined, VisibilityOutlined } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormHelperText,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Typography,
+} from "@mui/material";
+import { useState,  } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [passwordHelperText, setPasswordHelperText] = useState("");
+  const [email, setEmail] = useState("");
+  const [isEmailEmpty, setIsEmailEmpty] = useState(false);
+  const [emailHelperText, setEmailHelperText] = useState("");
+  const navigate = useNavigate();
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPasswordError(false);
+    setPassword(event.target.value);
+  };
+
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleEmailChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setEmail(event.target.value);
+    setIsEmailEmpty(false);
+  };
+
+  const handleLogin = () => {
+    if (email === "") {
+      setIsEmailEmpty(true);
+      setEmailHelperText("Mandotary fields missing");
+    }
+    if (password !== "correct password") {
+      setPasswordError(true);
+      setPasswordHelperText("Invalid email or password");
+    }
+
+    if (
+      email !== "" &&
+      password === "correct password" &&
+      !passwordError &&
+      !isEmailEmpty
+    ) {
+        console.log("Logged in");
+        navigate("/home");
+    }
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          maxWidth: "552px",
+          width: "calc(100% - 40px)",
+          height: "282px",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          padding: "0",
+        }}
+      >
+        <Box
+          sx={{
+            height: "32px",
+            padding: "16px",
+            display: "flex",
+            alignContent: "flex-start",
+          }}
+        >
+          <Typography
+            variant="h5"
+            sx={{
+              fontWeight: "400",
+              fontSize: "24px",
+              lineHeight: "32px",
+              textAlign: "center",
+              color: "rgba(0, 0, 0, 0.87)",
+              fontFamily: "Roboto, sans-serif",
+            }}
+          >
+            Login
+          </Typography>
+        </Box>
+        <form autoComplete="off">
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "16px",
+              gap: "16px",
+            }}
+          >
+            <FormControl fullWidth>
+              <InputLabel
+                size="small"
+                htmlFor="outlined-confirm-password"
+                color={isEmailEmpty ? "error" : "primary"}
+              >
+                Email
+              </InputLabel>
+              <OutlinedInput
+                size="small"
+                value={email}
+                onChange={handleEmailChange}
+                error={isEmailEmpty}
+                label="Email"
+                id="outlined-email"
+              />
+              {isEmailEmpty && (
+                <FormHelperText error>{emailHelperText}</FormHelperText>
+              )}
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel
+                size="small"
+                htmlFor="outlined-password"
+                color={passwordError ? "error" : "primary"}
+              >
+                Password
+              </InputLabel>
+              <OutlinedInput
+                size="small"
+                value={password}
+                onChange={handlePasswordChange}
+                error={passwordError}
+                label="Password"
+                id="outlined-password"
+                type={showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      edge="end"
+                    >
+                      {showPassword ? (
+                        <VisibilityOffOutlined />
+                      ) : (
+                        <VisibilityOutlined />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+              {passwordError && (
+                <FormHelperText error>{passwordHelperText}</FormHelperText>
+              )}
+            </FormControl>
+
+            <FormControl fullWidth>
+              <Button
+                variant="contained"
+                fullWidth
+                sx={{ backgroundColor: "rgba(33, 150, 243, 1)" }}
+                onClick={handleLogin}
+              >
+                Login
+              </Button>
+            </FormControl>
+          </Box>
+        </form>
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+          <Typography
+            sx={{
+                
+              fontSize: "13px",
+              fontWeight: "600",
+              color: "rgba(0, 0, 0, 0.6)",
+            }}
+          >
+            Don't have account?
+          </Typography>
+          <Button
+            onClick={() => navigate("/register")}
+            size="small"
+            sx={{
+              padding: "0 2px",
+              textTransform: "none",
+                fontWeight: "600",
+                fontSize: "13px",
+              color: "rgba(33, 150, 243, 1)",
+            }}
+          >
+            Register now
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default Login;
--- front-end/src/routes.tsx
+++ front-end/src/routes.tsx
@@ -3,11 +3,16 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import PasswordCreation from "./pages/PasswordCreation/PasswordCreation";
+import Login from "./pages/Lgin/Login";
+import Register from "./pages/Register/Register";
 
 const routes = (
   <Routes>
-    <Route path="/" element={<Home />} />
+    <Route path="/" element={<Login />} />
+    <Route path="/login" element={<Login />} />
+    <Route path="/home" element={<Home />} />
     <Route path="/password-creation" element={<PasswordCreation />} />
+    <Route path="/register" element={<Register />} />
   </Routes>
 );
 
